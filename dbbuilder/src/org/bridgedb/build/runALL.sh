8dd1fdb7b7bddfd3bde5b4e42a8f792110b4104b
AlexanderPico <AlexanderPico@e3f1d335-44b1-4163-9530-9b341189ae98>
2009-05-15 22:04:40 +0000

    combined ETL and Derby run scripts
    git-svn-id: http://svn.bigcat.unimaas.nl/bridgedb/trunk@29 e3f1d335-44b1-4163-9530-9b341189ae98

---
+++ dbbuilder/src/org/bridgedb/build/runALL.sh
@@ -0,0 +1,21 @@
+cat runList2 | while read x y
+do
+ # check if commented out
+ if [[ $x =~ '#' ]]
+ then
+        echo "Skip!: $x $y commented out in file: runList"
+        continue
+ fi
+
+  echo $x $y
+  (
+  cd /home/apico/EnsemblAPI
+  perl Ensembl_ETL_Device_v18_local.pl $x $y 
+  ) 
+  bash -x scripts/buildPathVisio.sh $x $y > temp.$x.$y 2> temp.$x.$y.err
+done
+
+# report from temp.err files
+grep -n ERROR temp.*.err
+
+
