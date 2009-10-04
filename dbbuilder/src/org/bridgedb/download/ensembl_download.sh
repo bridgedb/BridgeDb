01216c1b56fb6c577c209ed9e2067f60cb7a2566
AlexanderPico <AlexanderPico@e3f1d335-44b1-4163-9530-9b341189ae98>
2009-10-04 19:40:09 +0000

    Added support for funcgen download and installation
    git-svn-id: http://svn.bigcat.unimaas.nl/bridgedb/trunk@196 e3f1d335-44b1-4163-9530-9b341189ae98

--- dbbuilder/src/org/bridgedb/download/ensembl_download.sh
+++ dbbuilder/src/org/bridgedb/download/ensembl_download.sh
@@ -59,12 +59,13 @@ then
 
 fi  # end ontology grab
 
+  # grab core tables
   export species_name="$species_name"; var=`echo "ls -l /pub/current_mysql/" | ncftp ${ftp_site} | perl -ane 'if ( $_ =~ /$Logged in to/ ) { $go=1; next } if ( $go ) { @a=split; $fileName=$a[ scalar( @a ) - 1]; $compareStr=$ENV{ species_name }."_core"; if ( $fileName =~/^$compareStr/) { print $fileName."\n"; } }'`; set -- $var; mysql_db_name=$1 
 
   # check variable before proceeding
   if [[ $mysql_db_name =~ 'species_list' || $mysql_db_name == '' ]]
   then
-        echo "Skip!:  $species_name not found at $ftp_site. Please fix file: $species_list"
+        echo "Skip!:  $species_name _core not found at $ftp_site. Please fix file: $species_list"
         continue
   fi
 
@@ -98,6 +99,57 @@ fi  # end ontology grab
     #import data
     cp $table_name /tmp/
     
+    ${mysqlimport} -u genmapp -pfun4genmapp ${mysql_db_name} /tmp/${table_name}
+
+    rm /tmp/${table_name}
+  done
+ )
+
+# zip, archive and clean up
+gzip ./${mysql_db_name}.sql
+tar cvzf ./${mysql_db_name}.txt.tgz ./${mysql_db_name}/*
+rm -R ./${mysql_db_name}/
+
+  # grab funcgen tables
+  export species_name="$species_name"; var=`echo "ls -l /pub/current_mysql/" | ncftp ${ftp_site} | perl -ane 'if ( $_ =~ /$Logged in to/ ) { $go=1; next } if ( $go ) { @a=split; $fileName=$a[ scalar( @a ) - 1]; $compareStr=$ENV{ species_name }."_funcgen"; if ( $fileName =~/^$compareStr/) { print $fileName."\n"; } }'`; set -- $var; mysql_db_name=$1
+
+  # check variable before proceeding
+  if [[ $mysql_db_name =~ 'species_list' || $mysql_db_name == '' ]]
+  then
+        echo "Skip!:  $species_name _funcgen not found at $ftp_site. Please fix file: $species_list"
+        continue
+  fi
+
+  echo "** $mysql_db_name **"
+  mkdir ./${mysql_db_name}
+
+  # get raw tables from ftp site
+  ncftpget ${ftp_site} ./${mysql_db_name} /pub/current_mysql/${mysql_db_name}/*.txt.gz
+  for q in ./${mysql_db_name}/*.txt.gz
+  do
+    # skip huge, unused dna tables
+    if [[ $q =~ 'result' ]]
+    then
+        echo "Skip!: large, unused result table: $q"
+        continue
+    fi
+    gunzip $q
+  done
+
+  ncftpget ${ftp_site} ./ /pub/current_mysql/${mysql_db_name}/${mysql_db_name}.sql.gz
+  gunzip ./${mysql_db_name}.sql.gz
+
+  #create local mysql db
+  echo "create database if not exists ${mysql_db_name}" | ${mysql} -u genmapp -pfun4genmapp
+  #create schema
+  ${mysql} -u genmapp -pfun4genmapp ${mysql_db_name} < ./${mysql_db_name}.sql
+  #place in own scope due to "cd" command
+  (
+   cd ./${mysql_db_name}; for table_name in *.txt
+   do
+    #import data
+    cp $table_name /tmp/
+
     ${mysqlimport} -u genmapp -pfun4genmapp ${mysql_db_name} /tmp/${table_name}
 
     rm /tmp/${table_name}
