3e1e73dadec652d61d900d51e8f32b3e439cbddc
martijn <martijn@e3f1d335-44b1-4163-9530-9b341189ae98>
2009-11-09 14:10:44 +0000

    Added a few examples to demonstrate the working of the API
    git-svn-id: http://svn.bigcat.unimaas.nl/bridgedb/trunk@263 e3f1d335-44b1-4163-9530-9b341189ae98

---
+++ examples/src/org/bridgedb/examples/ExHello.java
@@ -0,0 +1,43 @@
+package org.bridgedb.examples;
+
+import java.util.Set;
+
+import org.bridgedb.BridgeDb;
+import org.bridgedb.DataSource;
+import org.bridgedb.IDMapper;
+import org.bridgedb.IDMapperException;
+import org.bridgedb.Xref;
+import org.bridgedb.bio.BioDataSource;
+
+public class ExHello 
+{
+
+	public static void main(String args[]) throws ClassNotFoundException, IDMapperException
+	{
+		// This example shows how to map an identifier
+		// using BridgeWebservice
+		
+		// first we have to load the driver
+		// and initialize information about DataSources
+		Class.forName("org.bridgedb.webservice.bridgerest.BridgeRest");
+		BioDataSource.init();
+		
+		// now we connect to the driver and create a IDMapper instance.
+		IDMapper mapper = BridgeDb.connect ("idmapper-bridgerest:http://webservice.bridgedb.org/Human");
+		
+		// We create an Xref instance for the identifier that we want to look up.
+		// In this case we want to look up Entrez gene 3643.
+		Xref src = new Xref ("3643", BioDataSource.ENTREZ_GENE);
+		
+		// let's see if there are cross-references to Ensembl Human
+		Set<Xref> dests = mapper.mapID(src, DataSource.getBySystemCode("EnHs"));
+		
+		// and print the results.
+		// with getURN we obtain valid MIRIAM urn's if possible.
+		System.out.println (src.getURN() + " maps to:");
+		for (Xref dest : dests)
+			System.out.println("  " + dest.getURN());
+		
+	}
+	
+}
---
+++ examples/src/org/bridgedb/examples/ExIDSearch.java
@@ -0,0 +1,38 @@
+package org.bridgedb.examples;
+
+import java.util.Set;
+
+import org.bridgedb.BridgeDb;
+import org.bridgedb.IDMapper;
+import org.bridgedb.IDMapperException;
+import org.bridgedb.Xref;
+import org.bridgedb.bio.BioDataSource;
+
+public class ExIDSearch 
+{
+
+	public static void main(String args[]) throws ClassNotFoundException, IDMapperException
+	{
+		// This example shows how to do a free text search for an identifier
+		
+		// first we have to load the driver
+		// and initialize information about DataSources
+		Class.forName("org.bridgedb.webservice.bridgerest.BridgeRest");
+		BioDataSource.init();
+		
+		// now we connect to the driver and create a IDMapper instance.
+		IDMapper mapper = BridgeDb.connect ("idmapper-bridgerest:http://webservice.bridgedb.org/Human");
+		
+		String query = "3643";
+			
+		// let's do a free search without specifying the input type:
+		Set<Xref> hits = mapper.freeSearch(query, 100);
+		
+		// Now print the results.
+		// with getURN we obtain valid MIRIAM urn's if possible.
+		System.out.println (query + " search results:");
+		for (Xref hit : hits)
+			System.out.println("  " + hit.getURN());
+	}
+	
+}
