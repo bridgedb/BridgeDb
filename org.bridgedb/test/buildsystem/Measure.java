eee4c7c4476e8ad4ac1bb3c4d96722a597937873
martijn <martijn@e3f1d335-44b1-4163-9530-9b341189ae98>
2010-03-22 13:53:00 +0000

    Source code for Measure.jar
    git-svn-id: http://svn.bigcat.unimaas.nl/bridgedb/trunk@332 e3f1d335-44b1-4163-9530-9b341189ae98

---
+++ org.bridgedb/test/buildsystem/Measure.java
@@ -0,0 +1,37 @@
+package buildsystem;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Measure
+{
+	private File dest;
+	
+	public Measure(String fileName)
+	{
+		dest = new File (System.getProperty("user.home"), fileName);
+	}
+	
+	public void add(String key, String value, String unit)
+	{
+		try
+		{
+			DateFormat fmt = new SimpleDateFormat("dd MMM yy HH:mm z");
+			// open file for appending.
+			FileWriter fw = new FileWriter (dest, true);
+			fw.write(fmt.format (new Date()) + "\t" + 
+					key + "\t" + 
+					value + "\t" + 
+					unit + "\n");
+			fw.close();			
+		}
+		catch (IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
