570b96b849dfe68db136702bed17abfd059adfcc
AlexanderPico <AlexanderPico@e3f1d335-44b1-4163-9530-9b341189ae98>
2010-06-23 21:29:14 +0000

    added funcgen dl
    git-svn-id: http://svn.bigcat.unimaas.nl/bridgedb/trunk@414 e3f1d335-44b1-4163-9530-9b341189ae98

--- dbbuilder/src/org/bridgedb/download/plants_download.sh
+++ dbbuilder/src/org/bridgedb/download/plants_download.sh
@@ -59,5 +59,45 @@ do
   tar cvzf ./${mysql_db_name}.txt.tgz ./${mysql_db_name}/*
   rm -R ./${mysql_db_name}/
 
+##FUNCGEN
+  export species_name="$species_name"; var=`echo "ls -l /pub/plants/release-${release}/mysql/" | ncftp ${ftp_site} | perl -ane 'if ( $_ =~ /$Logged in to/ ) { $go=1; next } if ( $go ) { @a=split; $fileName=$a[ scalar( @a ) - 1]; $compareStr=$ENV{ species_name }."_funcgen"; if ( $fileName =~/^$compareStr/) { print $fileName."\n"; } }'`; set -- $var; mysql_efg_name=$1
+
+  # check variable before proceeding
+  if [[ $mysql_efg_name =~ 'species_list' || $mysql_efg_name == '' ]]
+  then
+        echo "Skip!:  $species_name not found at $ftp_site. Please fix file: $species_list"
+        continue
+  fi
+
+  echo "** $mysql_efg_name **"
+  mkdir ./${mysql_efg_name}
+
+  # get raw tables from ftp site
+  ncftpget ${ftp_site} ./${mysql_efg_name} /pub/plants/release-${release}/mysql/${mysql_efg_name}/*.txt.gz
+  for q in ./${mysql_efg_name}/*.txt.gz
+  do
+    gunzip $q
+  done
+
+  ncftpget ${ftp_site} ./ /pub/plants/release-${release}/mysql/${mysql_efg_name}/${mysql_efg_name}.sql.gz
+  gunzip ./${mysql_efg_name}.sql.gz
+
+  #create local mysql db
+  echo "create database if not exists ${mysql_efg_name}" | ${mysql} -u genmapp -pfun4genmapp
+  #create schema
+  ${mysql} -u genmapp -pfun4genmapp ${mysql_efg_name} < ./${mysql_efg_name}.sql
+   # place in own scope, due to "cd" command
+  (
+    cd ./${mysql_efg_name}; for table_name in *.txt
+    do
+      ${mysqlimport} -u genmapp -pfun4genmapp ${mysql_efg_name} `pwd`/${table_name}
+    done
+  )
+
+  # zip, archive and clean up
+  gzip ./${mysql_efg_name}.sql
+  tar cvzf ./${mysql_efg_name}.txt.tgz ./${mysql_efg_name}/*
+  rm -R ./${mysql_efg_name}/
+
 done
 
