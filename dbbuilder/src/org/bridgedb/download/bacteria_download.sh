77f657cb342189e352d7fb7f70b1604533c36a8e
AlexanderPico <AlexanderPico@e3f1d335-44b1-4163-9530-9b341189ae98>
2010-06-23 21:16:39 +0000

    added release variable
    git-svn-id: http://svn.bigcat.unimaas.nl/bridgedb/trunk@413 e3f1d335-44b1-4163-9530-9b341189ae98

--- dbbuilder/src/org/bridgedb/download/bacteria_download.sh
+++ dbbuilder/src/org/bridgedb/download/bacteria_download.sh
@@ -1,6 +1,8 @@
 ##!!!!!!!!!!!
-# Update "release-#" below
+# Update release-#
 ##!!!!!!!!!!!
+
+release='5'
 
 ftp_site=ftp.ensemblgenomes.org
 species_list=${1?"Usage: $0 species_list"}
@@ -18,7 +20,7 @@ do
 	continue
  fi
 
-  export species_name="$species_name"; var=`echo "ls -l /pub/bacteria/release-4/mysql/" | ncftp ${ftp_site} | perl -ane 'if ( $_ =~ /$Logged in to/ ) { $go=1; next } if ( $go ) { @a=split; $fileName=$a[ scalar( @a ) - 1]; $compareStr=$ENV{ species_name }."_core"; if ( $fileName =~/^$compareStr/) { print $fileName."\n"; } }'`; set -- $var; mysql_db_name=$1
+  export species_name="$species_name"; var=`echo "ls -l /pub/bacteria/release-${release}/mysql/" | ncftp ${ftp_site} | perl -ane 'if ( $_ =~ /$Logged in to/ ) { $go=1; next } if ( $go ) { @a=split; $fileName=$a[ scalar( @a ) - 1]; $compareStr=$ENV{ species_name }."_core"; if ( $fileName =~/^$compareStr/) { print $fileName."\n"; } }'`; set -- $var; mysql_db_name=$1
 
   # check variable before proceeding
   if [[ $mysql_db_name =~ 'species_list' || $mysql_db_name == '' ]]
@@ -32,13 +34,13 @@ do
 
 
   # get raw tables from ftp site
-  ncftpget ${ftp_site} ./${mysql_db_name} /pub/bacteria/release-4/mysql/${mysql_db_name}/*.txt.gz
+  ncftpget ${ftp_site} ./${mysql_db_name} /pub/bacteria/release-${release}/mysql/${mysql_db_name}/*.txt.gz
   for q in ./${mysql_db_name}/*.txt.gz
   do
     gunzip $q
   done
 
-  ncftpget ${ftp_site} ./ /pub/bacteria/release-4/mysql/${mysql_db_name}/${mysql_db_name}.sql.gz
+  ncftpget ${ftp_site} ./ /pub/bacteria/release-${release}/mysql/${mysql_db_name}/${mysql_db_name}.sql.gz
   gunzip ./${mysql_db_name}.sql.gz
 
   #rename 3-name databases
--- dbbuilder/src/org/bridgedb/download/plants_download.sh
+++ dbbuilder/src/org/bridgedb/download/plants_download.sh
@@ -1,6 +1,8 @@
 ##!!!!!!!!!!!
-# Update "release-#" below
+# Update release-#
 ##!!!!!!!!!!!
+
+release='5'
 
 ftp_site=ftp.ensemblgenomes.org
 species_list=${1?"Usage: $0 species_list"}
@@ -18,7 +20,7 @@ do
 	continue
  fi
 
-  export species_name="$species_name"; var=`echo "ls -l /pub/plants/release-4/mysql/" | ncftp ${ftp_site} | perl -ane 'if ( $_ =~ /$Logged in to/ ) { $go=1; next } if ( $go ) { @a=split; $fileName=$a[ scalar( @a ) - 1]; $compareStr=$ENV{ species_name }."_core"; if ( $fileName =~/^$compareStr/) { print $fileName."\n"; } }'`; set -- $var; mysql_db_name=$1
+  export species_name="$species_name"; var=`echo "ls -l /pub/plants/release-${release}/mysql/" | ncftp ${ftp_site} | perl -ane 'if ( $_ =~ /$Logged in to/ ) { $go=1; next } if ( $go ) { @a=split; $fileName=$a[ scalar( @a ) - 1]; $compareStr=$ENV{ species_name }."_core"; if ( $fileName =~/^$compareStr/) { print $fileName."\n"; } }'`; set -- $var; mysql_db_name=$1
 
   # check variable before proceeding
   if [[ $mysql_db_name =~ 'species_list' || $mysql_db_name == '' ]]
@@ -30,15 +32,14 @@ do
   echo "** $mysql_db_name **"
   mkdir ./${mysql_db_name}
 
-
   # get raw tables from ftp site
-  ncftpget ${ftp_site} ./${mysql_db_name} /pub/plants/release-4/mysql/${mysql_db_name}/*.txt.gz
+  ncftpget ${ftp_site} ./${mysql_db_name} /pub/plants/release-${release}/mysql/${mysql_db_name}/*.txt.gz
   for q in ./${mysql_db_name}/*.txt.gz
   do
     gunzip $q
   done
 
-  ncftpget ${ftp_site} ./ /pub/plants/release-4/mysql/${mysql_db_name}/${mysql_db_name}.sql.gz
+  ncftpget ${ftp_site} ./ /pub/plants/release-${release}/mysql/${mysql_db_name}/${mysql_db_name}.sql.gz
   gunzip ./${mysql_db_name}.sql.gz
 
   #create local mysql db
