6e4c55f8180a21f15b6c7be62c4a25831c965079
Christian-B <brenninc@cs.man.ac.uk>
2012-03-02 10:59:05 +0000

    First attempt at IDMapper using linksets

---
+++ org.bridgedb.linkset/src/org/bridgedb/linkset/RdfConstants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bridgedb.linkset;
+
+/**
+ *
+ */
+class RdfConstants {
+
+    static final String TYPE =
+            "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+    
+}
