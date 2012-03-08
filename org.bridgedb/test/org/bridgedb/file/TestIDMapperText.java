328447fdcc297531bd26db29adccf7495f9da910
Christian-B <brenninc@cs.man.ac.uk>
2012-03-08 16:37:14 +0000

    Improved IDMapper tests

--- org.bridgedb/test/org/bridgedb/file/TestIDMapperText.java
+++ org.bridgedb/test/org/bridgedb/file/TestIDMapperText.java
@@ -3,6 +3,8 @@ package org.bridgedb.file;
 //import buildsystem.Measure;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import org.bridgedb.IDMapperException;
 import org.bridgedb.IDMapperTest;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -17,7 +19,7 @@ public class TestIDMapperText extends IDMapperTest {
 	private static final File INTERFACE_TEST_FILE = new File ("test-data/interfaceTest.txt");
 	
     @BeforeClass
-    public static void setUpClass() throws Exception {
+    public static void setupIDMapper() throws IDMapperException, MalformedURLException{
 		idMapper = new IDMapperText(INTERFACE_TEST_FILE.toURL());
 	}
 	
