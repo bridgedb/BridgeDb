93105ec9c213b9f1b95e4fa57801748687fd4a45
Christian-B <brenninc@cs.man.ac.uk>
2012-03-30 10:49:48 +0100

    Ops bundle project

--- org.bridgedb.url.sql/src/org/bridgedb/sql/LinksetLoader.java
+++ org.bridgedb.url.sql/src/org/bridgedb/sql/LinksetLoader.java
@@ -18,19 +18,21 @@ public class LinksetLoader {
     
    	public static void main(String[] args) 
             throws BridgeDbSqlException, IOException, RDFParseException, RDFHandlerException, IDMapperException {
-        if (args.length != 1){
+        if (args.length != 2){
             usage();
         }
         SQLAccess sqlAccess = URLSqlFactory.createSQLAccess();
         URLMapperSQL urlMapperSQL = new URLMapperSQL(sqlAccess);
         LinksetParser parser = new LinksetParser();
-        parser.parse (urlMapperSQL, args[0], "http://example.com");
+        parser.parse (urlMapperSQL, args[0], args[1]);
     }
 
     private static void usage() {
         System.out.println("Welcome to the OPS Linkset Loader.");
         System.out.println("This methods requires the file name (incl path) of the linkset to be loaded.");
-        System.out.println("Please run this again with a single paramter of the file name");
+        System.out.println("Please run this again with two paramters");
+        System.out.println("The file name (including path of the linkset");
+        System.out.println("The base uri for any ids without a base URI.");
         System.exit(1);
     }
 }
