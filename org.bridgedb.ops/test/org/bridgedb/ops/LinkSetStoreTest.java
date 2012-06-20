7f3799b030bb711431f0eb6b8c44a2812b9bb9fe
Christian-B <brenninc@cs.man.ac.uk>
2012-06-20 10:25:59 +0100

    Linkset Store work in progress

---
+++ org.bridgedb.ops/test/org/bridgedb/ops/LinkSetStoreTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bridgedb.ops;
+
+import java.util.List;
+import org.bridgedb.IDMapperException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Christian
+ */
+public class LinkSetStoreTest {
+    
+    static LinkSetStore instance;
+    
+    public LinkSetStoreTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        instance = new StubLinkSetStore();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getLinksetNames method, of class LinkSetStore.
+     */
+    @Test
+    public void testGetLinksetNames() throws Exception {
+        System.out.println("getLinksetNames");
+        List result = instance.getLinksetNames();
+        assertTrue(result.size() >= 3);
+    }
+
+    /**
+     * Test of getRDF method, of class LinkSetStore.
+     */
+    @Test
+    public void testGetRDF() throws Exception {
+        System.out.println("getRDF");
+        int id = 0;
+        String result = instance.getRDF(id);
+        assertNotNull(result);
+    }
+
+ }
