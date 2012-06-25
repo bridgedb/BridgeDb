a4e7c82d5d23307c39c4067e3c4ca94835179c86
Christian-B <brenninc@cs.man.ac.uk>
2012-06-25 16:40:29 +0100

    Added more linkset validation

--- org.bridgedb.linksets/src/org/bridgedb/linkset/constants/RdfConstants.java
+++ org.bridgedb.linksets/src/org/bridgedb/linkset/constants/RdfConstants.java
@@ -2,14 +2,17 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.bridgedb.linkset;
+package org.bridgedb.linkset.constants;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
 
 /**
  *
  */
-class RdfConstants {
+public class RdfConstants {
 
     static final String TYPE =
             "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
-    
+    public static final URI TYPE_URI = new URIImpl(TYPE);
 }
