342c9e209527d9d5a06590b5aac7726bd44ab163
Christian-B <brenninc@cs.man.ac.uk>
2012-07-04 15:54:41 +0100

    new SQl to IDMapper Capabilities

---
+++ org.bridgedb.sql2/test/org/bridgedb/mysql/IDMapperCapabilitiesTest.java
@@ -0,0 +1,21 @@
+package org.bridgedb.mysql;
+
+import org.bridgedb.IDMapperException;
+import org.bridgedb.sql.SQLAccess;
+import org.bridgedb.sql.TestSqlFactory;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+
+/**
+ * 
+ * @author Christian
+ */
+public class IDMapperCapabilitiesTest extends org.bridgedb.IDMapperCapabilitiesTest {
+    
+    @BeforeClass
+    public static void setupIDMapper() throws IDMapperException{
+        SQLAccess sqlAccess = TestSqlFactory.createTestSQLAccess();
+        capabilities = new MySQLMapper(sqlAccess);
+     }
+            
+}
