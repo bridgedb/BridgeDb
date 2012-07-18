16dfb49d2313dad0d7baab1ccd54fa2f00783ab2
Christian-B <brenninc@cs.man.ac.uk>
2012-07-18 18:37:23 +0200

    Documentation changes

--- org.bridgedb.ops.sql/src/org/bridgedb/url/URLListener.java
+++ org.bridgedb.ops.sql/src/org/bridgedb/url/URLListener.java
@@ -1,25 +1,80 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.bridgedb.url;
 
 import org.bridgedb.DataSource;
 import org.bridgedb.IDMapperException;
 
 /**
- *
+ * Import Interface that supports the adding of URLs.
+ * 
  * @author Christian
  */
 public interface URLListener {
+    
+    /**
+     * Associates a DataSource with a UriSpace.
+     * <p>
+     * This adds the UriSpace as an alternative without removing any previous ones or changing the default/ primary UriSpace.
+     * 
+     * @param source A BridgeDB DataSource to associate with.
+     * @param uriSpace A UriSpace to add to
+     * @throws IDMapperException 
+     */
     public void registerUriSpace(DataSource source, String uriSpace) throws IDMapperException;
 
-    public int registerMappingSet(String sourceUriSpace, String predicate, String targetUriSpace, 
+    //Note: To change the primary UriSpace you need to change the DataSource. 
+    
+    //Note: to unregister a uriSpace you currently have to use an SQL intervention.
+    
+    /**
+     * Registers a mapping set using UriSpaces rather than DataSoucres.
+     * <p>
+     * Similar to MappingListener.registerMappingSet(DataSource source, String predicate, DataSource target, 
+     *    boolean symetric, boolean isTransitive)
+     * In fact implementations are encouraged to obtain the DataSources and call the MappingListener method.
+     * @param sourceUriSpace A registered UriSpace used by the source URLS
+      * @param predicate The predicate to be associated. Can be null
+     * @param targetUriSpace A registered UriSpace used by the source URLS
+     * @param symetric Flag to say if mapping should be loaded one way of both ways. 
+     *     Creates two mapping sets this one and the inverse with one number higher.
+     * @param transative Flag to indicate if the mapping was created using transativity
+     * @return Id of the forward mappingSet.
+     * @throws IDMapperException Thrown if either UriSpace has not previously been registered using registerUriSpace
+     */
+   public int registerMappingSet(String sourceUriSpace, String predicate, String targetUriSpace, 
         boolean symetric, boolean transative) throws IDMapperException;
 
-    public void insertURLMapping(String sourceURL, String targetURL, int mappingSet, boolean symetric) 
+    /**
+     * Inserts a mapping into the system.
+     * <p>
+     * Similar to MappingListener.insertLink(String sourceId, String targetId, int mappingSet, boolean symetric)
+     * In fact implementations are encouraged to obtain the ids and call the MappingListener method.
+     * <p>
+     * For speed the implementations are not required to verify that the UriSpace part of the URLs matches the de
+     * <p>
+     * Implementatins may buffer the inserts, so closeInput must be called after the last insert or inserts may be lost. 
+     * <p>
+     * No checking can be done that the DataSource part of the Xrefs match the declaration.
+     * <p>
+     * For speed, the implemented methods is also not required to check that the MappingsSet exists 
+     * nor that the mapping Set's semantic setting matches that in the insert call. 
+     * 
+     * @param sourceId ID of the source xref
+     * @param targetId ID of the target xref
+     * @param mappingSet The ID of the mapping set to be inserted into.
+     * @param symetric If true the inverse mapping will be inserted into the mapping set one number higher.
+     * @throws IDMapperException 
+     */
+   public void insertURLMapping(String sourceURL, String targetURL, int mappingSet, boolean symetric) 
             throws IDMapperException;
     
-    public void closeInput()throws IDMapperException;
+    /**
+     * Closes the input, flushing any links into storage.
+     * <p>
+     * May also update any cashed counts ext.
+     * <p>
+     * This method and MappingListener method are intended to be duplicates of each other.
+     * @throws IDMapperException 
+     */
+   public void closeInput()throws IDMapperException;
 
 }
