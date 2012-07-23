d57ac5c56caaa04bbe37e584a345e9a0e3eec4c9
Christian-B <brenninc@cs.man.ac.uk>
2012-07-23 15:52:12 +0100

    bunmped version number of core modules.

--- org.bridgedb.webservice.cronos/src-axis/org/bridgedb/webservice/cronos/ObjectFactory.java
+++ org.bridgedb.webservice.cronos/src-axis/org/bridgedb/webservice/cronos/ObjectFactory.java
@@ -37,22 +37,6 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link IsinRedList }
-     * 
-     */
-    public IsinRedList createIsinRedList() {
-        return new IsinRedList();
-    }
-
-    /**
-     * Create an instance of {@link CronosWS_Type }
-     * 
-     */
-    public CronosWS_Type createCronosWS_Type() {
-        return new CronosWS_Type();
-    }
-
-    /**
      * Create an instance of {@link CronosWSResponse }
      * 
      */
@@ -66,6 +50,22 @@ public class ObjectFactory {
      */
     public IsinRedListResponse createIsinRedListResponse() {
         return new IsinRedListResponse();
+    }
+
+    /**
+     * Create an instance of {@link IsinRedList }
+     * 
+     */
+    public IsinRedList createIsinRedList() {
+        return new IsinRedList();
+    }
+
+    /**
+     * Create an instance of {@link CronosWS_Type }
+     * 
+     */
+    public CronosWS_Type createCronosWS_Type() {
+        return new CronosWS_Type();
     }
 
     /**
