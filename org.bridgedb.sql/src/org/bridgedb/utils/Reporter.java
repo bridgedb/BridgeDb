e10a933701ec8df4528bbdeda354f5aec3820ef7
AlasdairGray <A.Gray@cs.man.ac.uk>
2012-08-06 15:05:34 +0100

    Validates the existence for the VoID description predicates.
    Does not validate the types of the values.

--- org.bridgedb.sql/src/org/bridgedb/utils/Reporter.java
+++ org.bridgedb.sql/src/org/bridgedb/utils/Reporter.java
@@ -3,7 +3,7 @@ package org.bridgedb.utils;
 /**
  * Util functions that allows messages to be output.
  * <p>
- * Allows the output format to be changed in one place so chaning everywhere.
+ * Allows the output format to be changed in one place so changing everywhere.
  * <p>
  * All other System.out calls can then be considered debug commands that should not have stayed in.
  * 
