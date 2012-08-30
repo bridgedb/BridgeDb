d024f0f10c34a68a59f8ba7199958df2403a340d
Christian-B <brenninc@cs.man.ac.uk>
2012-08-30 13:40:39 +0200

    Added Linkset metadata

--- org.bridgedb.linksets/src/org/bridgedb/metadata/LinkSetMetaData.java
+++ org.bridgedb.linksets/src/org/bridgedb/metadata/LinkSetMetaData.java
@@ -4,26 +4,67 @@
  */
 package org.bridgedb.metadata;
 
+import java.util.Date;
+import java.util.HashSet;
+import org.bridgedb.linkset.constants.DctermsConstants;
+import org.bridgedb.linkset.constants.PavConstants;
 import org.bridgedb.linkset.constants.VoidConstants;
 import org.openrdf.model.Resource;
 import org.openrdf.model.URI;
+import org.openrdf.model.Value;
 
 /**
  *
  * @author Christian
  */
-public class LinkSetMetaData extends MetaData{
+public class LinkSetMetaData extends DataSetCollectionMetaData{
 
     DataSetMetaData sourceDataSet;
-    DataSetMetaData targetDataSet;
+    DataSetMetaData objectDataSet;
+    //Add them to DataSetCollectionMetaData.dataSets as well to pick up code there
     
     public LinkSetMetaData(Resource id, RDFData input){
-        super(id, input);
+        super(id, input);        
+    }
+
+    public LinkSetMetaData(RDFData input){
+        super(input);        
     }
 
     @Override
     void setupValues() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        values.add(new SingletonValue("License", DctermsConstants.LICENSE, URI.class, RequirementLevel.MUST));
+        values.add(new SingletonValue("Authored By",PavConstants.AUTHORED_BY, URI.class, RequirementLevel.MUST));
+        values.add(new SingletonValue("Authored On",PavConstants.AUTHORED_ON, Date.class, RequirementLevel.MUST));
+        values.add(new SingletonValue("Created By",PavConstants.CREATED_BY, URI.class, RequirementLevel.MUST));
+        values.add(new SingletonValue("Created On",PavConstants.CREATED_ON, Date.class, RequirementLevel.MUST));
+        values.add(new SingletonValue("Predicate",VoidConstants.LINK_PREDICATE, URI.class, RequirementLevel.TECHNICAL_MUST));
+        values.add(new SingletonValue("Number of Links",VoidConstants.TRIPLES, Integer.class, RequirementLevel.MUST));
+        values.add(new SingletonValue("Title", DctermsConstants.TITLE, String.class, RequirementLevel.MUST));
+        values.add(new SingletonValue("Description", DctermsConstants.DESCRIPTION, String.class, RequirementLevel.MUST));
+    }
+
+    @Override
+    void readFromInput(RDFData input) {
+        if (dataSets == null){
+            dataSets = new HashSet<DataSetMetaData>(); 
+        }
+        //Read source And Object DataSets first to stop super from reading them as just datasets. 
+        Value sourceId = this.getByIdPredicate(input, VoidConstants.SUBJECTSTARGET);   
+        if (sourceId != null && sourceId instanceof Resource){
+            sourceDataSet = new DataSetMetaData((Resource)sourceId, input);
+            dataSets.add(sourceDataSet);
+        } else {
+            sourceDataSet = null;
+        }
+        Value objectId = this.getByIdPredicate(input, VoidConstants.OBJECTSTARGET);   
+        if (objectId != null && objectId instanceof Resource){
+            objectDataSet = new DataSetMetaData((Resource)objectId, input);
+            dataSets.add(objectDataSet);
+        } else {
+            objectDataSet = null;
+        }
+        super.readFromInput(input);
     }
 
     @Override
@@ -31,4 +72,21 @@ public class LinkSetMetaData extends MetaData{
          return VoidConstants.LINKSET;
     }
     
+    public boolean hasRequiredValues(RequirementLevel forceLevel, boolean exceptAlternatives){
+        if (this.sourceDataSet == null){ return false; }
+        if (this.objectDataSet == null){ return false; }
+        //Super will check the datasets as long as they are there.
+        return super.hasRequiredValues(forceLevel, exceptAlternatives);
+    }
+
+    void validityReport(StringBuilder builder, RequirementLevel forceLevel, boolean exceptAlternatives, 
+            boolean includeWarnings){
+        if (this.sourceDataSet == null){         
+            builder.append("ERROR: sourceDataSet not found. \n");
+        }
+        if (this.objectDataSet == null){         
+            builder.append("ERROR: objectDataSet not found. \n");
+        }
+        super.validityReport(builder, forceLevel, exceptAlternatives, includeWarnings);
+    }
 }
