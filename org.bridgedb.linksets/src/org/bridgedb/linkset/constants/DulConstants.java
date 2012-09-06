68292aaed63699ee2d88f6b332aa66c41a6edf16
Christian-B <brenninc@cs.man.ac.uk>
2012-09-06 15:00:11 +0100

    Bug fix in constant

--- org.bridgedb.linksets/src/org/bridgedb/linkset/constants/DulConstants.java
+++ org.bridgedb.linksets/src/org/bridgedb/linkset/constants/DulConstants.java
@@ -23,7 +23,7 @@ import org.openrdf.model.impl.URIImpl;
 
 public class DulConstants {
 
-	private static final String dulns = "<http://www.ontologydesignpatterns.org/ont/dul/DUL.owl";
+	private static final String dulns = "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#";
 	
 	public static final URI EXPRESSES = new URIImpl(dulns + "expresses");
     
