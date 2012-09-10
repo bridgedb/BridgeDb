7fb742bbf68353de6d324dc78458f4311881904f
Christian-B <brenninc@cs.man.ac.uk>
2012-09-10 14:07:32 +0100

    added correctType

---
+++ org.bridgedb.metadata/src/org/bridgedb/metadata/type/MetaDataType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bridgedb.metadata.type;
+
+import org.openrdf.model.Value;
+
+/**
+ *
+ * @author Christian
+ */
+public interface MetaDataType {
+    
+    boolean correctType(Value value);
+}
---
+++ org.bridgedb.metadata/src/org/bridgedb/metadata/type/UriType.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bridgedb.metadata.type;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+
+/**
+ *
+ * @author Christian
+ */
+public class UriType implements MetaDataType{
+
+    @Override
+    public boolean correctType(Value value) {
+        return value instanceof URI;
+    }
+    
+}
