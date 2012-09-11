f66923b0fcaf4467940dbd830617173d72b6c90d
Christian-B <brenninc@cs.man.ac.uk>
2012-09-11 14:21:44 +0100

    Added DataSet2 to tests

---
+++ org.bridgedb.metadata/test/org/bridgedb/metadata/DataSet2MetaDataTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bridgedb.metadata;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import org.bridgedb.metadata.utils.Reporter;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Christian
+ */
+public class DataSet2MetaDataTest extends MetaDataTestBase{
+    
+    public DataSet2MetaDataTest() throws DatatypeConfigurationException{        
+    }
+    
+    @Test
+    public void testHasRequiredValues() throws MetaDataException{
+        Reporter.report("HasRequiredValues");
+        MetaDataCollection metaData = new MetaDataCollection(loadDataSet2());
+        checkRequiredValues(metaData, RequirementLevel.MUST);
+        assertFalse(metaData.hasRequiredValues(RequirementLevel.MAY));
+    } 
+
+    @Test
+    public void testHasCorrectTypes() throws MetaDataException{
+        Reporter.report("HasCorrectTypes");
+        MetaDataCollection metaData = new MetaDataCollection(loadDataSet2());
+        checkCorrectTypes(metaData);
+    }
+    
+}
