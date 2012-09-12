dd8df37677e06445cc0114abcc30d7d010709e49
Christian-B <brenninc@cs.man.ac.uk>
2012-09-12 14:49:37 +0100

    Updated and tested allowed types

--- org.bridgedb.metadata/src/org/bridgedb/metadata/type/AllowedValueType.java
+++ org.bridgedb.metadata/src/org/bridgedb/metadata/type/AllowedValueType.java
@@ -7,8 +7,10 @@ package org.bridgedb.metadata.type;
 import java.util.ArrayList;
 import java.util.List;
 import org.bridgedb.metadata.AppendBase;
+import org.bridgedb.metadata.constants.SchemaConstants;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -20,7 +22,8 @@ public class AllowedValueType implements MetaDataType{
 
     List<String> allowedValues = new ArrayList<String>();
     
-    public AllowedValueType(NodeList list){
+    public AllowedValueType(Element element){
+        NodeList list = element.getElementsByTagName(SchemaConstants.ALLOWED_VALUE);
         for (int i = 0; i < list.getLength(); i++){
             Node node = list.item(i);
             allowedValues.add(node.getFirstChild().getNodeValue());
---
+++ org.bridgedb.metadata/test/org/bridgedb/metadata/type/AllowedValueTypeTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bridgedb.metadata.type;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.bridgedb.metadata.utils.Reporter;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.LiteralImpl;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author Christian
+ */
+public class AllowedValueTypeTest {
+    
+    static Element property;
+    
+    public AllowedValueTypeTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        //Create instance of DocumentBuilderFactory
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        //Get the DocumentBuilder
+        DocumentBuilder parser = factory.newDocumentBuilder();
+        //Create blank DOM Document 
+        Document doc = parser.newDocument();
+        property = doc.createElement("property");
+        Element child1 = doc.createElement("AllowedValue");
+        child1.appendChild(doc.createTextNode("first"));
+        property.appendChild(child1);
+        Element child2 = doc.createElement("AllowedValue");
+        child2.appendChild(doc.createTextNode("second"));
+        property.appendChild(child2);
+        Element child3 = doc.createElement("AllowedValue");
+        child3.appendChild(doc.createTextNode("third"));
+        property.appendChild(child3);
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of correctType method, of class IntegerType.
+     */
+    @Test
+    public void testCorrectType() {
+        Reporter.report("correctType");
+        AllowedValueType instance = new AllowedValueType(property);
+        Value value = new LiteralImpl("first");
+        assertTrue(instance.correctType(value));
+        value = new LiteralImpl("1st");
+        assertFalse(instance.correctType(value));
+    }
+
+    /**
+     * Test of getCorrectType method, of class IntegerType.
+     */
+    @Test
+    public void testGetCorrectType() {
+        System.out.println("getCorrectType");
+        AllowedValueType instance = new AllowedValueType(property);
+        String expResult = " One of [first, second, third]";
+        String result = instance.getCorrectType();
+        assertEquals(expResult, result);
+    }
+}
