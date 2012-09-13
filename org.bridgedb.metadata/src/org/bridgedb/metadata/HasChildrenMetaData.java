f8063667826bc5e0addc3887ad5aeece6d36b39f
Christian-B <brenninc@cs.man.ac.uk>
2012-09-13 15:04:26 +0100

    Subsets but getting data from parents in progress

---
+++ org.bridgedb.metadata/src/org/bridgedb/metadata/HasChildrenMetaData.java
@@ -0,0 +1,137 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bridgedb.metadata;
+
+import java.util.List;
+import java.util.Set;
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.w3c.dom.Element;
+
+/**
+ * 
+ * @author Christian
+ */
+public abstract class HasChildrenMetaData  extends MetaDataBase implements MetaData{
+    
+    final List<MetaDataBase> childMetaData;
+
+    HasChildrenMetaData(Element element) throws MetaDataException{
+        super(element);
+        childMetaData = MetaDataRegistry.getChildMetaData(element);
+    }
+
+    HasChildrenMetaData(String name, List<MetaDataBase> childMetaData){
+        super(name);
+        this.childMetaData = childMetaData;
+    }
+    
+    // ** MetaData methods 
+    
+    @Override
+    public Set<Value> getValuesByPredicate(URI predicate) {
+        for (MetaDataBase child:childMetaData){
+            Set<Value> possible = child.getValuesByPredicate(predicate);
+            if (possible != null){
+                return possible;
+            }
+        }
+        return null;
+    }
+    
+    @Override
+    public boolean hasCorrectTypes() {
+        for (MetaDataBase child:childMetaData){
+            if (!child.hasCorrectTypes()){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean hasRequiredValues(RequirementLevel requirementLevel) {
+        for (MetaDataBase child:childMetaData){
+            if (!child.hasRequiredValues(requirementLevel)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean allStatementsUsed() {
+        //A group has values only if all children do.
+        for (MetaDataBase child:childMetaData){
+            if (!child.allStatementsUsed()) { 
+                return false; 
+            }
+        }
+        return true;
+    }
+    
+    // ** MetaDataBase methods 
+    
+    @Override
+    void loadValues(Resource id, Set<Statement> data, MetaData parent) {
+        setupValues(id, parent);
+        for (MetaDataBase child:childMetaData){
+            child.loadValues(id, data, this);
+        }
+    }
+
+    @Override
+    boolean hasValues() {
+        for (MetaDataBase child:childMetaData){
+            if (!child.hasValues()) { 
+                return false; 
+            }
+        }
+        return true;
+    }
+    
+    // ** AppendBase methods 
+    
+    @Override
+    void appendSchema(StringBuilder builder, int tabLevel) {
+        appendSpecific(builder, tabLevel);
+        for (MetaDataBase child:childMetaData){
+            child.appendSchema(builder, tabLevel + 1);
+        }
+    }
+
+   @Override
+    void appendShowAll(StringBuilder builder, RequirementLevel forceLevel, int tabLevel) {
+        appendSpecific(builder, tabLevel);
+        for (MetaDataBase child:childMetaData){
+            child.appendShowAll(builder, forceLevel, tabLevel + 1);
+        }
+    }
+
+    @Override
+    void appendUnusedStatements(StringBuilder builder) {
+        for (MetaDataBase child:childMetaData){
+            child.appendUnusedStatements(builder);
+        }
+    }
+
+    // ** Abstract methods used above
+    abstract void appendSpecific(StringBuilder builder, int tabLevel);
+    
+    /* Methods with no shared implementation 
+    @Override
+    MetaDataBase getSchemaClone() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    void appendValidityReport(StringBuilder builder, RequirementLevel forceLevel, boolean includeWarnings, int tabLevel) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    */
+    
+}
--- org.bridgedb.metadata/src/org/bridgedb/metadata/constants/VoidConstants.java
+++ org.bridgedb.metadata/src/org/bridgedb/metadata/constants/VoidConstants.java
@@ -38,7 +38,7 @@ public class VoidConstants {
     public static final URI TRIPLES = new URIImpl(voidns + "triples");
     public static final URI SUBJECTSTARGET = new URIImpl(voidns + "subjectsTarget");
     public static final URI OBJECTSTARGET = new URIImpl(voidns + "objectsTarget");
-    //public static final String SUBSET = voidns + "subset";
+    public static final URI SUBSET = new URIImpl(voidns + "subset");
     public static final URI SPARQL_ENDPOINT = new URIImpl(voidns + "sparqlEndpoint");
     public static final URI URI_SPACE = new URIImpl(voidns + "uriSpace");
     public static final URI VOCABULARY = new URIImpl(voidns + "vocabulary");
