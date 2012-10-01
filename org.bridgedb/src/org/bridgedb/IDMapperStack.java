5d4917a3ebcc324aa35218c6909811414fc11033
martijn <martijn@e3f1d335-44b1-4163-9530-9b341189ae98>
2012-10-01 19:55:09 +0000

    Finalized integration of Jahn's transitive mapping work. Junit tests are now working
    git-svn-id: http://svn.bigcat.unimaas.nl/bridgedb/trunk@655 e3f1d335-44b1-4163-9530-9b341189ae98

--- org.bridgedb/src/org/bridgedb/IDMapperStack.java
+++ org.bridgedb/src/org/bridgedb/IDMapperStack.java
@@ -16,7 +16,6 @@
 //
 package org.bridgedb;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -25,8 +24,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Vector;
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.bridgedb.impl.TransitiveGraph;
 
 /**
  * combines multiple {@link IDMapper}'s in a stack.
@@ -58,17 +58,18 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class IDMapperStack implements IDMapper, AttributeMapper
 {
-	private List<IDMapper> gdbs = new CopyOnWriteArrayList<IDMapper>();
+	// reference shared with TransitiveGraph
+	private final List<IDMapper> gdbs = new CopyOnWriteArrayList<IDMapper>();
 
-	private Map<DataSource, PathCollection> mappingGraph; // paths hashed by
-																// the the
-																// paths' source
-																// (starting
-																// node)
-	private Map<DataSource, PathCollection> targetMap; // paths hashed by
-															// the the paths'
-															// target (terminal
-															// node)
+	/** Helper class for calculating transitive paths */
+	private TransitiveGraph transitiveGraph = null;
+	
+	private TransitiveGraph getTransitiveGraph() throws IDMapperException
+	{
+		if (transitiveGraph == null)
+			transitiveGraph = new TransitiveGraph(gdbs);
+		return transitiveGraph;
+	}
 
 	/**
 	 * Create a fresh IDMapper from a connectionString and add it to the stack.
@@ -91,12 +92,7 @@ public class IDMapperStack implements IDMapper, AttributeMapper
     {
         if (idMapper!=null) {
             gdbs.add(idMapper);
-            try {
-				rebuildDataSourcesMap(); // re-build the mapping graph
-			} catch (IDMapperException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            transitiveGraph = null; // trigger rebuild.
         }
     }
     
@@ -129,12 +125,7 @@ public class IDMapperStack implements IDMapper, AttributeMapper
     public void removeIDMapper(IDMapper idMapper)
     {
     	gdbs.remove(idMapper);
-    	try {
-			rebuildDataSourcesMap();
-		} catch (IDMapperException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	transitiveGraph = null; // trigger rebuild
     }
 
 	/**
@@ -241,24 +232,10 @@ public class IDMapperStack implements IDMapper, AttributeMapper
 		/** {@inheritDoc} */
 		public boolean isMappingSupported(DataSource src, DataSource tgt)
 				throws IDMapperException {
-				
-			if(isTransitive){	
-				if(IDMapperStack.this.mappingGraph==null){
-					IDMapperStack.this.rebuildDataSourcesMap();
-				}
-				
-				if( mappingGraph.get(src)==null ) {
-					return false; 
-				}
-
-				for (Path path : mappingGraph.get(src).closedList) {
-					if (path.size() > 0) {
-						Edge lastEdge = path.get(path.size() - 1);
-						if (lastEdge.target == tgt) {
-							return true;
-						}
-					}
-				}
+			
+			if(isTransitive)
+			{
+				return getTransitiveGraph().isTransitiveMappingSupported(src, tgt);
 			}
 			
 			else {
@@ -269,8 +246,7 @@ public class IDMapperStack implements IDMapper, AttributeMapper
 			}
 			}
 			return false;
-		}
-		
+		}		
 		
 		/**
 		 * @return true if free search is supported by one of the children
@@ -534,453 +510,19 @@ public class IDMapperStack implements IDMapper, AttributeMapper
 	private Set<Xref> mapIDtransitive(Xref ref, DataSource... resultDs)
 			throws IDMapperException {
 		
-		// first round
-		Set<Xref> result = new HashSet<Xref>();
-		Set<DataSource> dsFilter = new HashSet<DataSource>(Arrays
-				.asList(resultDs));
-		DataSource dataSource = ref.getDataSource();
-
-		if( resultDs.length == 0 ) {
+		if( resultDs.length == 0 ) 
+		{
+			return getTransitiveGraph().mapIDtransitiveUntargetted(ref);
+		}
+		else
+		{
+			Set<DataSource> dsFilter = new HashSet<DataSource>(Arrays
+					.asList(resultDs));
+			Set<Xref> result = getTransitiveGraph().mapIDtransitiveTargetted(ref, dsFilter);
 			return result;
 		}
-				
-		if (targetMap == null) {
-			rebuildDataSourcesMap();
-		}
-
-		for (DataSource key : targetMap.keySet()) {
-			for (Path path : targetMap.get(key).closedList) {
-				if (path.size() > 0) {
-					Edge lastEdge = path.get(path.size() - 1);
-					DataSource firstDataSource = path.get(0).source;
-					DataSource lastTarget = lastEdge.target;
-					if ( firstDataSource == dataSource && key==lastTarget && 
-							dsFilter.contains(lastTarget)) 
-					{
-						for (Xref j : mapID(ref, path))
-						{
-							if (dsFilter.contains(j.getDataSource())) 
-								result.add(j);
-						}
-					}
-				}
-			}
-		}
-
-		return result;
 	}
 
-		
-	/**
-	 * 
-	 * Map ID transitively using specified sequence of DataSources given by path.
-	 * 
-	 * @param Xref
-	 *            Xref to be mapped
-	 * @param path
-	 *            Sequence of DataSources to be mapped, given as a Path.
-	 */
-	private Set<Xref> mapID(Xref xref, Path path) throws IDMapperException {
-		Set<Xref> result = new HashSet<Xref>();
-		
-		if (path == null || path.size() <= 0) {
-			return result;
-		}
-		
-		Edge e = path.get(0);
-		result = e.label.mapID(xref, e.target);
-
-		for (int i = 1; i < path.size(); i++) {
-			e = path.get(i);
-			if( e == null || e.source == null || e.target == null || e.label == null ) {
-				throw new IDMapperException();
-			}
-			if( ! e.label.isConnected() ) {
-				return new HashSet<Xref>();
-			}
-			Map<Xref, Set<Xref>> tmp = e.label.mapID(result, e.target);
-			result.clear();
-			for (Xref key : tmp.keySet()) {				
-				result.addAll(tmp.get(key));
-			}
-		}
-		
-		return result;
-	}
-
-	
-	// convenience method
-	private Set<Xref> getSubsetMatchingDataSource( Set<Xref> set, DataSource source ) {
-		Set<Xref> subset = new HashSet<Xref>();
-		for( Xref x: subset ) {
-			if( x.getDataSource() == source ) {
-				subset.add(x);
-			}
-		}
-		return subset;
-	}
-	
-	
-	/**
-	 * Initialize the mapGraph before calling rebuildPath().
-	 * 
-	 * @return Hash that contains all relevant information on maps between
-	 *         DataSources of all IDMappers in this IDMapperStack. Reflexive
-	 *         maps (DataSourced X -> DataSource X) are ignored. The map will
-	 *         contain only DataSources that are connected.
-	 *         
-	 * @throws IDMapperException
-	 */
-	private Map<DataSource, PathCollection> initMap()
-			throws IDMapperException {
-
-		mappingGraph = new HashMap<DataSource, PathCollection>();
-
-		// add each DataSource to a PathCollection
-		for (IDMapper idm : gdbs) {
-			if (idm != null && idm.isConnected()) {
-				IDMapperCapabilities capas = idm.getCapabilities();
-				for (DataSource src : capas.getSupportedSrcDataSources()) {
-					for (DataSource tgt : capas.getSupportedTgtDataSources()) {
-						if (capas.isMappingSupported(src, tgt) && src != tgt) {
-							Edge edge = new Edge(src, tgt, idm);
-							Path path = new Path();
-							path.add(edge);
-							if (mappingGraph.get(src) == null) {
-								PathCollection paths = new PathCollection(src);
-								paths.openList = new ArrayList<Path>();
-								paths.openList.add(path);
-								mappingGraph.put(src, paths);
-							} else {
-								mappingGraph.get(src).openList.add(path);
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return mappingGraph;
-	}
-
-
-	/**
-	 *  Populate the targetMap. the targetMap allows to look up
-	 *  a list of all paths that end on a given target.
-	 **/
-	private void rebuildTargetMap() {
-		
-		targetMap = new HashMap<DataSource,PathCollection>();
-		
-		for( DataSource key : mappingGraph.keySet() ){
-			for ( Path path : mappingGraph.get(key).closedList ) {
-				if( path.size() > 0 ) {
-					Edge lastEdge = path.get( path.size() - 1);;
-					DataSource tgt = lastEdge.target;
-					List<Path> list;
-					if(targetMap.get(tgt)==null) {
-						PathCollection collection = new PathCollection(tgt);
-						targetMap.put( tgt, collection );
-						list = collection.closedList;
-					}
-					else {
-						list = targetMap.get(tgt).closedList;
-					}	
-					list.add(path);
-				}
-			}
-		}
-	}
-	
-
-	
-	/**
-	 * Rebuild the DataSources map that contains information on all mappings
-	 * supported by this IDMapperStack.
-	 * 
-	 * @throws IDMapperException
-	 * 
-	 * */
-	public void rebuildDataSourcesMap() throws IDMapperException {
-
-		initMap(); // initialize map
-		boolean isDone = false;
-		while (!isDone) { // keep adding paths while there are new Edges for the
-							// graph
-			isDone = rebuildPaths();
-		}
-		rebuildTargetMap();
-	}
-
-	
-	/** 
-	 * Rebuild the path information for transitive maps.
-	 *
-	 * Calculate all non-cyclic paths on the connection graph. 
-	 * Each path connects two DataSources. This is the implementation
-	 * of the algorithm for finding all paths we are interested in.
-	 * 
-	 * @param true, if path-building process is completed, i.e., 
-	 *        if all open lists are empty.
-	 */
-	private boolean rebuildPaths() {
-		
-		HashMap<DataSource,List<Path>> extendedPaths = new HashMap<DataSource,List<Path>>(); 
-		
-		// create extended paths for every DataSource
-		for( DataSource key : mappingGraph.keySet() ) {
-			
-			PathCollection collection = mappingGraph.get(key);
-			List<Path> extendedPathsForDataSource = new ArrayList<Path>();
-
-			for (Path path : collection.openList) {
-
-				DataSource lastElement = path.get(path.size() - 1).target;
-				List<Path> extensions = mappingGraph.get(lastElement).getExtendingPaths(path);
-					
-				path.toBeMoved = true;
-				for( Path extension : extensions ) {
-					// add extending Path to openList
-					Path newPath = new Path( path, extension );
-					if( !collection.contains(newPath) ) {
-						path.toBeMoved = false;
-						if(!extendedPathsForDataSource.contains( newPath )){
-							extendedPathsForDataSource.add( newPath );
-						}
-					}
-				}		
-			}
-			
-			if(extendedPathsForDataSource.size()>0){
-				extendedPaths.put( key, extendedPathsForDataSource );
-			}
-		}
-		
-		/* update map in two steps:
-		 *      (1) move paths from open list to closed list,
-		 *      (2) add new extended paths into open list. 
-		 */
-		
-		// move marked paths from open list to closed list
-		for( DataSource key : mappingGraph.keySet() ) {
-			PathCollection collection = mappingGraph.get(key);
-			List<Path> openList = collection.openList;
-			List<Path> closedList = collection.closedList;
-			List<Path> swapList = new ArrayList<Path>();
-			for( Path p : openList ) {
-				if(p.toBeMoved) {
-					closedList.add(p);
-				}
-				else {
-					swapList.add(p);
-				}
-			}
-			collection.openList = swapList;
-		}
-		
-
-		// move new extended paths into open list
-		for( DataSource key : extendedPaths.keySet() ) {
-			for( Path extendedPath : extendedPaths.get(key) ) {
-				mappingGraph.get(key).openList.add(extendedPath); 
-			}
-		}
-		
-		// check for empty list
-		boolean allOpenListsEmpty = true;
-		for( DataSource key : mappingGraph.keySet() ) {
-			if( !mappingGraph.get(key).openList.isEmpty() ) {
-				allOpenListsEmpty = false;
-				break;
-			}
-		}
-		
-		return allOpenListsEmpty;   // the map is complete, when all openList are empty
-	}
-
-
-	/** 
-	 * This is for testing. Removed as soon as the code is considered mature.
-	 */
-	private void printMap( HashMap<DataSource,PathCollection> map) {
-		for( DataSource ds : map.keySet() ) {
-			PathCollection collection = map.get(ds);
-			System.out.println("DataSource: " + ds);
-			System.out.println("OL: " + collection.openList);
-			System.out.println("CL: " + collection.closedList + "\n");
-		}
-		System.out.println();		
-	}
-	
-
-	/* An Edge is an edge in the graph that represents all possible,
-	 * transitive (and loop free) mappings supported by the IDMapper Stack.
-	 * An edge connects two DataSources (source and target) by an IDMapper
-	 * from the gdbs. Every Edge is directed to point from source to target.
-	 * Note that for internal convenience, two Edges are considered equal
-	 * if they connect the same DataSources irrespective of the direction
-	 * of the Edge. 
-	 */
-	protected class Edge {
-		DataSource source;
-		DataSource target;
-		IDMapper label;
-		
-		public Edge(DataSource source, DataSource target, IDMapper label) {
-			this.source = source;
-			this.target = target;
-			this.label = label;
-		}
-
-		public String toString() {
-			return " -> " + target;
-		}
-		
-		
-		/** This Edge is equal to another object, if the other object is an Edge
-		 *  and both Edges have the same label AND also the same nodes. The direction
-		 *  of the Edges does not matter.
-		 *  
-		 *  @param o Another Edge
-		 */
-		public boolean equals( Object o ) {
-			if( ! (o instanceof Edge) ) {
-				return false;
-			}
-			Edge e = (Edge) o;
-			return (this.label == e.label && this.source == e.source && this.target == e.target)
-					|| (this.label == e.label && this.source == e.target && this.target == e.source);			
-		}
-		
-	}
-	
-	/**
-	 *  A Path is a vector of Edges in the graph that describe the relationships between the
-	 *  (transitive) maps supported by the IDMapperStack. 
-	 * 
-	 */
-	protected class Path extends Vector<Edge> {
-		
-		private static final long serialVersionUID = 1L;
-
-		protected boolean toBeMoved = false;  // flag for iterations in rebuilPath()
-
-		protected Path() {
-			super();
-		}
-		
-		protected Path( Path p, Path q ) {
-			addAll(p);
-			addAll(q);
-		}
-
-		/**
-		 * @param other Some other path
-		 * @return true iff this path contains not element of the other path
-		 */
-		protected boolean isLoopFreeExtension( Path other ) {
-
-			boolean returnValue = true;
-			for( Edge e : other ){
-				if( this.contains( e ) ) {
-					returnValue = false;
-					break;
-				}
-			}
-			return returnValue;
-		}
-		
-		public String toString(){
-			String returnValue = "";
-			if(!isEmpty()){
-				returnValue += this.get(0).source;
-			}
-			for ( Edge e : this ) {
-					returnValue += e;
-			}
-			return returnValue;
-		}
-		
-		
-		/**
-		 *  @param other Other path to be compared to.
-		 *  @return true iff this path is equal to the other path.
-		 *  	Two pathes are equal, if they have the same number of edges,
-		 *  	each edge has the same source, target and label, and 
-		 *  	the edges occur in the same order.
-		 */
-		public boolean equals( Object other ) {
-			if( !(other instanceof Path) ) {
-				return false;
-			}
-			Path path = (Path) other;
-			if( this.size() != path.size() ) {
-				return false;
-			}
-			
-			boolean returnValue = true;
-			for (int i = 0; i < size(); i++) {
-				Edge myEdge = get(i);
-				Edge otherEdge = path.get(i);
-				if (myEdge.source != otherEdge.source
-						|| myEdge.target != otherEdge.target
-						|| myEdge.label != otherEdge.label) {
-					returnValue = false;
-					break;
-				}
-			}
-			return returnValue;
-		}
-		
-	}
-	
-	
-	/**
-	 * This is class is a data structure for collecting all possible
-	 * transitive, non-loopy mappings supported by this IDMapperStack.
-	 */
-	protected class PathCollection {
-		public List<Path> openList;
-		public List<Path> closedList;
-		DataSource dataSource;
-		
-		public PathCollection( DataSource dataSource ) {
-			this.dataSource = dataSource;
-			openList = new ArrayList<Path>();
-			closedList = new ArrayList<Path>();
-		}
-		
-		/** True if equal path is already contained in open or closed list. */
-		public boolean contains(Path path) {
-			return openList.contains(path) || closedList.contains(path);
-		}
-
-		public String toString(){
-			return "OL: " + openList + "; CL: " + closedList;
-		}
-		
-		public List<Path> getExtendingPaths( Path otherPath ) {
-
-			List<Path> list = new ArrayList<Path>();			
-
-			for( Path p : openList ) {
-				if( p.isLoopFreeExtension(otherPath) ) {
-					list.add(p);
-				}
-			}
-
-			for( Path p : closedList ) {
-				if( p.isLoopFreeExtension(otherPath) ) {
-					list.add(p);
-				}
-			}
-
-			return list;
-		}
-		
-	}
-	
-	
 	/**
 	 * helper method to map Id's in transitive mode.
 	 * @param ref Xref to map
@@ -1044,11 +586,11 @@ public class IDMapperStack implements IDMapper, AttributeMapper
 		}
 		return result;
 	}
-
 	
 	/** get all mappers */
 	public List<IDMapper> getMappers()
 	{
 		return gdbs;
 	}
+
 }
