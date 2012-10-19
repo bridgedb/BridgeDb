bf1f9e58a050024b4a575c70a7a6a6be8d579487
AlasdairGray <A.Gray@cs.man.ac.uk>
2012-10-19 09:27:55 +0100

    Test for creating web service with no parameters. Other tests rely on web service instantiated with Test Database

--- org.bridgedb.ops.ws.server/test/org/bridgedb/ws/server/WSOpsServiceTest.java
+++ org.bridgedb.ops.ws.server/test/org/bridgedb/ws/server/WSOpsServiceTest.java
@@ -1,26 +1,44 @@
 package org.bridgedb.ws.server;
 
 import org.bridgedb.IDMapperException;
+import org.bridgedb.mysql.MySQLSpecific;
+import org.bridgedb.sql.SQLAccess;
+import org.bridgedb.sql.SQLUrlMapper;
+import org.bridgedb.sql.TestSqlFactory;
+import org.bridgedb.url.URLMapper;
 import org.bridgedb.ws.WSOpsService;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WSOpsServiceTest {
 	
+	private WSOpsService service;
+
+	@Before
+	public void serviceSetup() throws IDMapperException {
+		SQLAccess sqlAccess = TestSqlFactory.createTestSQLAccess();
+		URLMapper urlMapper = new SQLUrlMapper(false, sqlAccess, new MySQLSpecific());
+		service = new WSOpsService(urlMapper);
+	}
+	
+	@Test
+	public void testEmptySetup() throws IDMapperException {
+		WSOpsService serviceDefault = new WSOpsService();
+		serviceDefault.mapURL("http://www.foo.com/123", "", null);
+	}
+	
 	@Test(expected=IDMapperException.class)
 	public void testMapUrlNoArguments() throws IDMapperException {
-		WSOpsService service = new WSOpsService();
 		service.mapURL(null, null, null);
 	}
 	
 	@Test(expected=IDMapperException.class)
 	public void testMapUrlEmptyArguments() throws IDMapperException {
-		WSOpsService service = new WSOpsService();
 		service.mapURL("", "", null);
 	}
 	
 	@Test
 	public void testMapUrlEmptyProfile() throws IDMapperException {
-		WSOpsService service = new WSOpsService();
 		service.mapURL("http://www.foo.com/123", null, null);
 	}
 	
