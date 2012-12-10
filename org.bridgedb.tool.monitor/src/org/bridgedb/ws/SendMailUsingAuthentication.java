b259c29bd808e41e123ba7a96e0226dcfee3d692
Christian-B <brenninc@cs.man.ac.uk>
2012-12-10 09:45:51 +0000

    cleanup of sending

--- org.bridgedb.tool.monitor/src/org/bridgedb/ws/SendMailUsingAuthentication.java
+++ org.bridgedb.tool.monitor/src/org/bridgedb/ws/SendMailUsingAuthentication.java
@@ -24,19 +24,10 @@ import org.bridgedb.utils.ConfigReader;
 public class SendMailUsingAuthentication
 {
 
-  private static final String SMTP_HOST_NAME1 = "mail.smtp.host"; //Outgoing.manchester.ac.uk"; //"post.demon.co.uk";
-  private static final String SMTP_AUTH_USER1 = "smpt.auth.user"; //"mbaxecb2"; //"meijer1";
-  private static final String SMTP_AUTH_PWD1  = "smpt.auth.password"; //One+1+4=Six"; //skilift";
-
   private Properties properties;
   
-  //private static final String emailMsgTxt      = "Hi, \n I am looking for a sewing machine for my neice. \n Thanks Christian";
-  //private static final String emailSubjectTxt  = "[wilmslow_freecycle] WANTED Sewing Machine (Wilmslow)";
-
   // Add List of Email address to who email needs to be sent to
   private static final String[] emailList = {"cyb@cs.man.ac.uk", "test1@brenn.co.uk"};
-
-  private static final String fileName = "c:\\DropBox\\Mailer\\addresses.csv";
 
   public SendMailUsingAuthentication() throws IDMapperException{
       properties = ConfigReader.getProperties("mailerConfig.properties");
@@ -80,40 +71,6 @@ public class SendMailUsingAuthentication
     msg.setSubject(subject);
     msg.setContent(message, "text/plain");
     Transport.send(msg);
- }
-
-  public void autoPostMail(String subject, String message , String fileName) throws MessagingException, FileNotFoundException, IOException
-  {
-     boolean debug = false;
-
-    Authenticator auth = new SMTPAuthenticator();
-    Session session = Session.getDefaultInstance(properties, auth);
-
-    session.setDebug(debug);
-
-    File file = new File(fileName);
-    BufferedReader reader = new BufferedReader(new FileReader(file));
-    String line;
-    while((line=reader.readLine())!=null) {
-
-        String[] addresses = line.split(",");
-        // create a message
-        Message msg = new MimeMessage(session);
-
-        System.out.print("Writing to "+addresses[0]);
-        // set the from and to address
-        InternetAddress addressFrom = new InternetAddress(addresses[1]);
-        msg.setFrom(addressFrom);
-
-        InternetAddress addressTo = new InternetAddress(addresses[0]);
-        msg.setRecipient(Message.RecipientType.TO, addressTo);
-
-        // Setting the Subject and Content Type
-        msg.setSubject(subject);
-        msg.setContent(message, "text/plain");
-        Transport.send(msg);
-        System.out.println(" done");
-    }
  }
 
 /**
