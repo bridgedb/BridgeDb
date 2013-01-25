cc039f066300f8a5f5d3f623a3336d84c42e950a
Christian-B <brenninc@cs.man.ac.uk>
2013-01-25 13:37:59 +0000

    Added tool to create RDF including Primary DataSource info

---
+++ org.bridgedb.rdf/src/org/bridgedb/rdf/CreateRdf.java
@@ -0,0 +1,74 @@
+// BridgeDb,
+// An abstraction layer for identifier mapping services, both local and online.
+//
+// Copyright 2006-2009  BridgeDb developers
+// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+// Copyright 2012-2013  OpenPhacts
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package org.bridgedb.rdf;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import org.bridgedb.DataSource;
+import org.bridgedb.DataSourceOverwriteLevel;
+import org.bridgedb.IDMapper;
+import org.bridgedb.IDMapperException;
+import org.bridgedb.bio.BioDataSource;
+import org.bridgedb.utils.ConfigReader;
+
+/**
+ *
+ * @author Christian
+ */
+public class CreateRdf {
+    
+    private IDMapper mapper;
+    private String name;
+    private BufferedWriter buffer;
+    private String sourceUriSpace;
+    private String targetUriSpace;
+    private final String LINK_PREDICATE = "skos:relatedMatch";
+    
+    static final Logger logger = Logger.getLogger(CreateRdf.class);
+
+    public static void main(String[] args) throws IDMapperException, IOException, ClassNotFoundException{
+        ConfigReader.logToConsole();
+
+        //Load DataSources from BioDataSource and save that
+        DataSource.setOverwriteLevel(DataSourceOverwriteLevel.CONTROLLED);
+        BioDataSource.init();
+        File biofile = new File("../org.bridgedb.utils/resources/BioDataSource.ttl");
+        BridgeDBRdfHandler.writeRdfToFile(biofile, false);
+        DataSource.setOverwriteLevel(DataSourceOverwriteLevel.STRICT);
+        BridgeDBRdfHandler.parseRdfFile(biofile);        
+        
+        //Load the existing DataSource file 
+        File dsfile = new File("../org.bridgedb.utils/resources/DataSource.ttl");
+        DataSource.setOverwriteLevel(DataSourceOverwriteLevel.STRICT);
+        BridgeDBRdfHandler.parseRdfFile(dsfile);             
+        System.out.println("Parsing finished");
+        
+        //Write a file marking the primary URIs as currently set
+        File primaryfile = new File("../org.bridgedb.utils/resources/PrimaryDataSource.ttl");
+        BridgeDBRdfHandler.writeRdfToFile(primaryfile, true);
+        
+        DataSource.setOverwriteLevel(DataSourceOverwriteLevel.STRICT);
+        BridgeDBRdfHandler.parseRdfFile(primaryfile);
+        
+     }
+
+}
