24df7fa6bd122fd96ce55a5626bd04e1d1cf6f36
Christian-B <brenninc@cs.man.ac.uk>
2013-02-26 17:41:05 +0000

    Improved error reporting on test

--- org.bridgedb.tools.metadata/test/org/bridgedb/tools/metadata/TestUtils.java
+++ org.bridgedb.tools.metadata/test/org/bridgedb/tools/metadata/TestUtils.java
@@ -67,6 +67,7 @@ public abstract class TestUtils extends org.bridgedb.utils.TestUtils{
         if (!ok){
             //This test will fail but with extra info
             String report = metaData.validityReport(NO_WARNINGS);
+            report(report);
             assertThat(report, not(containsString("ERROR")));
             assertTrue(ok);
         }        
