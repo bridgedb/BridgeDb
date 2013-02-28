d99beb069db6455f244ce4d26658e074b2dcc46e
Christian-B <brenninc@cs.man.ac.uk>
2013-02-28 12:10:05 +0000

    Removed hard codes password (which has changed)

--- org.bridgedb.tools.metadata/src/org/bridgedb/rdf/reader/UrlReader.java
+++ org.bridgedb.tools.metadata/src/org/bridgedb/rdf/reader/UrlReader.java
@@ -12,6 +12,8 @@ import java.net.ProtocolException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import org.apache.commons.codec.binary.Base64;
+import org.bridgedb.utils.AccountInfo;
+import org.bridgedb.utils.AccountsReader;
 import org.bridgedb.utils.BridgeDBException;
 
 
@@ -27,9 +29,6 @@ public class UrlReader {
     private String RAW_GITHUB = "https://raw.github.com";
     private String HTML_GITHUB = "https://github.com";
     private String BLOB = "blob/";
-    private String encodedAuthorization;
-    private String login = "OpsReadOnly";
-    private String password = "Gerhard1";
     private String method = "GET";
     
     public UrlReader(String address) throws BridgeDBException{
@@ -41,8 +40,6 @@ public class UrlReader {
                 scrubbedAddress = scrubbedAddress.replaceFirst(BLOB, "");
             }
         }
-        String authorization = login + ':'  + password;
-        encodedAuthorization = new String(Base64.encodeBase64(authorization.getBytes()));
     }
     
     public final String getPath() throws BridgeDBException{
@@ -55,15 +52,28 @@ public class UrlReader {
         return uri.getScheme() + ":" + uri.getSchemeSpecificPart();
     }
 
+    private String getAuthorization(URL url) throws BridgeDBException{
+        AccountInfo info = AccountsReader.findByUrl(url);
+        if (info == null || info.getLogin() == null || info.getPassword() == null){
+            return null;
+        }
+        String authorization = info.getLogin() + ':'  + info.getPassword();
+        String encodedAuthorization = new String(Base64.encodeBase64(authorization.getBytes()));
+        return encodedAuthorization;
+    }
+    
     //taken from https://github.com/kohsuke/github-api
     //http://opensource.org/licenses/mit-license.php
-    private HttpURLConnection setupConnection(URL url) throws IOException {
+    private HttpURLConnection setupConnection(URL url) throws IOException, BridgeDBException {
         HttpURLConnection uc = (HttpURLConnection) url.openConnection();
 
-        // if the authentication is needed but no credential is given, try it anyway (so that some calls
-        // that do work with anonymous access in the reduced form should still work.)
-        // if OAuth token is present, it'll be set in the URL, so need to set the Authorization header
-        uc.setRequestProperty("Authorization", "Basic " + encodedAuthorization);
+        String encodedAuthorization = getAuthorization(url);
+        if (encodedAuthorization != null){
+            // if the authentication is needed but no credential is given, try it anyway (so that some calls
+            // that do work with anonymous access in the reduced form should still work.)
+            // if OAuth token is present, it'll be set in the URL, so need to set the Authorization header
+            uc.setRequestProperty("Authorization", "Basic " + encodedAuthorization);
+        }
 
         try {
             uc.setRequestMethod(method);
@@ -81,7 +91,7 @@ public class UrlReader {
         return uc;
     }
     
-    public InputStream getInputStream() throws IOException{
+    public InputStream getInputStream() throws IOException, BridgeDBException{
         System.out.println(scrubbedAddress);
         URL url = new URL(scrubbedAddress);
         HttpURLConnection uc = setupConnection(url);
