8cc570bb982362752b171cb97787508ceea8c1b7
Christian-B <brenninc@cs.man.ac.uk>
2013-03-05 17:21:29 +0000

    Home page with styles. other links to do.

--- org.bridgedb.uri.ws.server/src/org/bridgedb/ws/uri/WSOpsServer.java
+++ org.bridgedb.uri.ws.server/src/org/bridgedb/ws/uri/WSOpsServer.java
@@ -51,7 +51,7 @@ public class WSOpsServer extends WSLinksetService{
         logger.info("WsOpsServer setup");        
     }
     
-    private final void statsBlock(StringBuilder sb) throws BridgeDBException {
+    private final void statsBlock1(StringBuilder sb) throws BridgeDBException {
         OverallStatistics statistics = uriMapper.getOverallStatistics();
         sb.append("<div id=\"navBar\"><h2>Service statistics</h2>");
         sb.append("<ul>");
@@ -79,6 +79,23 @@ public class WSOpsServer extends WSLinksetService{
         sb.append("</ul></p></div>\n");        
     }
     
+        /**
+     * Allows Super classes to add to the side bar
+     */
+    protected void statsBlock(StringBuilder sb) throws BridgeDBException{
+        OverallStatistics statistics = uriMapper.getOverallStatistics();
+        sb.append("<div id=\"navBar\"><h2>Service statistics</h2>");
+        sb.append("\n<div class=\"menugroup\">Statisitics</div>");
+        addSideBarItem(sb, "getMappingInfo", formatter.format(statistics.getNumberOfMappings()) + " Mappings");
+        addSideBarItem(sb, "getMappingInfo", formatter.format(statistics.getNumberOfMappingSets()) + " Mapping Sets");
+        addSideBarItem(sb, "getSupportedSrcDataSources", formatter.format(statistics.getNumberOfSourceDataSources()) 
+                + " Source Data Sources");
+        addSideBarItem(sb, "getMappingInfo", formatter.format(statistics.getNumberOfPredicates()) + " Predicates");
+        addSideBarItem(sb, "getSupportedTgtDataSources", formatter.format(statistics.getNumberOfTargetDataSources()) 
+                + " Target Data Sources ");
+        sb.append("</div>\n");        
+    }
+
     private final void uriMappingForm(StringBuilder sb) throws BridgeDBException {
     	sb.append("<form method=\"get\" action=\"/");
         sb.append(getServiceName());
@@ -168,26 +185,79 @@ public class WSOpsServer extends WSLinksetService{
     protected StringBuilder header(String header) throws BridgeDBException{
         StringBuilder sb = new StringBuilder();
         sb.append("<?xml version=\"1.0\"?>");
-        sb.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
-                + "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+        sb.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" ");
+        sb.append("\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
         sb.append("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">");
         sb.append("<meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\"/>");
-        sb.append("<head>" +
-        		"<title>OPS IMS</title>" +
-        		"<style>" +
-        		"#container { width: 90%; margin: 10px auto; background-color: #fff; color: #333; border: 1px solid gray; line-height: 130%; font-family: perpetua, garamond, serif; font-size: 110%; min-width: 40em; }" +
-        		"#top { padding: .5em; background-color: #808080; border-bottom: 1px solid gray; }" +
-        		"#top h1 { padding: .25em .5em .25em .5em; margin-left: 200px; margin-bottom: 0; margin-right: 0; margin-top: 0 }" +
-        		"#top a { text-decoration: none; color: #ffffff; }"+
-        		"#navBar { float: left; width: 200px; margin: 0em; padding: 5px; min-width: 200px; border-right: 1px solid gray; } " +
-        		"#content { margin-left: 210px; border-left: 1px solid gray; padding: 1em; min-width: 20em; min-height: 500px; }" +
-        		"fieldset {border: 1px solid #781351;width: 20em}" + 
-        		"legend { color: #fff; background: #ffa20c; border: 1px solid #781351; padding: 2px 6px }" +
-        		"</style>" +
-        		"</head><body>");
+        sb.append("<head>\n");
+        sb.append("<title>OPS IMS</title>\n");
+        sb.append("<style>\n");
+        sb.append("#container { width: 90%; margin: 10px auto; background-color: #fff; color: #333; border: ");
+            sb.append("1px solid gray; line-height: 130%; font-family: perpetua, garamond, serif; font-size: 110%; ");
+            sb.append("min-width: 40em; }\n");
+        sb.append("#top { padding: .5em; background-color: #808080; border-bottom: 1px solid gray; }\n");
+        sb.append("#top h1 { padding: .25em .5em .25em .5em; margin-left: 200px; margin-bottom: 0; margin-right: 0; margin-top: 0 }\n");
+        sb.append("#top a { text-decoration: none; color: #ffffff; }\n");
+        sb.append("#navBar { float: left; width: 200px; margin: 0em; padding: 5px; min-width: 200px; border-right: 1px solid gray; } \n");
+        sb.append("#content { margin-left: 210px; border-left: 1px solid gray; padding: 1em; min-width: 20em; min-height: 500px; }\n");
+        sb.append("fieldset {border: 1px solid #781351;width: 20em}\n");
+        sb.append("legend { color: #fff; background: #ffa20c; border: 1px solid #781351; padding: 2px 6px }\n");
+        sb.append("</style>\n");
+        sb.append("<style type=\"text/css\">");
+        sb.append("	.texthotlink, .texthotlink_hilight { width: 150px; font-size: 85%; padding: .25em; cursor: ");
+            sb.append("pointer; color: black; font-family: Arial, sans-serif;	}\n");
+        sb.append("	.texthotlink_hilight {background-color: #fff6ac;}\n");
+        sb.append("		.menugroup { font-size: 90%; font-weight: bold; padding-top: .25em; }\n");
+        sb.append("		input { background-color: #EEEEFF; } body, td { background-color: white; font-family: sans-serif; }\n");
+        sb.append("	</style>\n");            
+        toggler(sb);
+        sb.append("</head><body>");
         return sb;
     }
-    
+
+    protected void toggler(StringBuilder sb) throws BridgeDBException{
+        sb.append("<script language=\"javascript\">\n");
+        sb.append("		function getObj(id) {\n");
+        sb.append("			return document.getElementById(id)\n");
+        sb.append("		}\n");
+        sb.append("		function DHTML_TextHilight(id) {\n");
+        sb.append("			getObj(id).classNameOld = getObj(id).className;\n");
+        sb.append("			getObj(id).className = getObj(id).className + \"_hilight\";\n");
+        sb.append("		}\n");
+        sb.append("		function DHTML_TextRestore(id) {\n");
+        sb.append("			if (getObj(id).classNameOld != \"\")\n");
+        sb.append("				getObj(id).className = getObj(id).classNameOld;\n");
+        sb.append("		}\n");
+        sb.append("     function getItem(id){\n");
+        sb.append("         var itm = false;\n");
+        sb.append("         if(document.getElementById)\n");
+        sb.append("             itm = document.getElementById(id);\n");
+        sb.append("         else if(document.all)\n");
+        sb.append("             itm = document.all[id];\n");
+        sb.append("         else if(document.layers)\n");
+        sb.append("             itm = document.layers[id];\n");
+        sb.append("         return itm;\n");
+        sb.append("    }\n\n");
+        sb.append("    function toggleItem(id)\n");
+        sb.append("{\n");
+        sb.append("    itm = getItem(id);\n");
+        sb.append("    if(!itm)\n");
+        sb.append("        return false;\n");
+        sb.append("    if(itm.style.display == 'none')\n");
+        sb.append("        itm.style.display = '';\n");
+        sb.append("    else\n");
+        sb.append("        itm.style.display = 'none';\n");
+        sb.append("    return false;\n");
+        sb.append("}\n\n");
+        sb.append("function hideDetails()\n");
+        sb.append("{\n");
+        sb.append("     toggleItem('ops')\n");
+        sb.append("     toggleItem('sparql')\n");
+        sb.append("     return true;\n");
+        sb.append("}\n\n");
+        sb.append("</script>\n");
+    }
+
     protected void top(StringBuilder sb, String header) throws BridgeDBException{
         sb.append("<div id=\"container\">");
         sb.append("<div id=\"top\">");
@@ -203,7 +273,7 @@ public class WSOpsServer extends WSLinksetService{
         sb.append("</div>");   
     }
     
-    /**
+     /**
      * Forwarding page for "/api".
      * 
      * This is expected to be overwirriten by the QueryExpander
