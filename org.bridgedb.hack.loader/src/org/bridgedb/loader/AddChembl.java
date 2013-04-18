a7794d70c94278eb6b1a9391854fde87c1c4ddad
Christian-B <brenninc@cs.man.ac.uk>
2013-04-18 14:39:45 +0100

    Updated chembi loader

--- org.bridgedb.hack.loader/src/org/bridgedb/loader/AddChembl.java
+++ org.bridgedb.hack.loader/src/org/bridgedb/loader/AddChembl.java
@@ -21,12 +21,16 @@ package org.bridgedb.loader;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import org.bridgedb.linkset.LinksetLoader;
 import org.bridgedb.linkset.transative.TransativeCreator;
 import org.bridgedb.linkset.transative.TransativeFinder;
+import org.bridgedb.sql.SQLUriMapper;
+import org.bridgedb.statistics.MappingSetInfo;
 import org.bridgedb.tools.metadata.validator.ValidationType;
 import org.bridgedb.utils.BridgeDBException;
 import org.bridgedb.utils.ConfigReader;
+import org.bridgedb.utils.Reporter;
 import org.bridgedb.utils.StoreType;
 import org.openrdf.model.URI;
 import org.openrdf.rio.RDFHandlerException;
@@ -39,13 +43,45 @@ public class AddChembl {
 
     private static final boolean LOAD = true;
     
+    private static int doTransative(LinksetLoader linksetLoader, int leftId, int rightId, StoreType storeType) throws BridgeDBException, RDFHandlerException, IOException{
+       SQLUriMapper mapper = SQLUriMapper.factory(false, storeType);  
+       MappingSetInfo left = mapper.getMappingSetInfo(leftId);
+       MappingSetInfo right = mapper.getMappingSetInfo(rightId);
+       File fileName = TransativeCreator.doTransativeIfPossible(left, right, storeType);
+       if (fileName == null){
+            Reporter.println ("No transative links found");
+            return -1;
+       } else {
+            Reporter.println("Created " + fileName);
+            HashSet<Integer> chainIds = TransativeFinder.getChain(left, right);
+            int dataSet =  linksetLoader.loadLinkset(fileName.getAbsolutePath(), storeType, ValidationType.LINKSMINIMAL, chainIds);
+            System.out.println("Loaded " + dataSet);
+            return dataSet;
+        }        
+    }
+    
     public static void main(String[] args) throws BridgeDBException, RDFHandlerException, IOException  {
         ConfigReader.logToConsole();
 
         LinksetLoader linksetLoader = new LinksetLoader();
+        TransativeFinder transativeFinder = new TransativeFinder(StoreType.LOAD);
 
- //       linksetLoader.load("https://github.com/openphacts/ops-platform-setup/blob/master/void/chebi/chebi102/ChEBI102VoID.ttl", 
- //               StoreType.LOAD, ValidationType.VOID);
+        //From fresh
+        linksetLoader.load("https://www.dropbox.com/sh/6dov4e3drd2nvs7/8RQlU-RH7m/ChemSpider-DrugBankDrugs.ttl", 
+                StoreType.LOAD, ValidationType.LINKSMINIMAL);
+        linksetLoader.load("https://www.dropbox.com/sh/6dov4e3drd2nvs7/W-nCm0Hnng/ConceptWiki-ChemSpider.ttl", 
+                StoreType.LOAD, ValidationType.LINKSMINIMAL);
+        transativeFinder.UpdateTransative();
+        
+        //New
+        //CS-> Chebi
+        linksetLoader.clearExistingData(StoreType.LOAD);
+                linksetLoader.load("https://www.dropbox.com/s/0w7tgw4zyou5aqs/LINKSET_EXACTMATCH_CHEBI20121023.ttl", 
+                StoreType.LOAD, ValidationType.LINKSMINIMAL);
+        transativeFinder.UpdateTransative();
+        //Chebi -> chebi
+        linksetLoader.load("https://github.com/openphacts/ops-platform-setup/blob/master/void/chebi/chebi102/ChEBI102VoID.ttl", 
+                StoreType.LOAD, ValidationType.VOID);
         linksetLoader.load("https://github.com/openphacts/ops-platform-setup/blob/master/void/chebi/chebi102/has_functional_parentChEBI102Linkset.ttl", 
                 StoreType.LOAD, ValidationType.LINKSMINIMAL);
         linksetLoader.load("https://github.com/openphacts/ops-platform-setup/blob/master/void/chebi/chebi102/has_parent_hydrideChEBI102Linkset.ttl", 
@@ -62,10 +98,8 @@ public class AddChembl {
                 StoreType.LOAD, ValidationType.LINKSMINIMAL);
         linksetLoader.load("https://github.com/openphacts/ops-platform-setup/blob/master/void/chebi/chebi102/is_tautomer_ofChEBI102Linkset.ttl", 
                 StoreType.LOAD, ValidationType.LINKSMINIMAL);
-  
-        TransativeFinder transativeFinder = new TransativeFinder(StoreType.LOAD);
-        transativeFinder.UpdateTransative();
 
-    }
+        transativeFinder.UpdateTransative();
+     }
 
 }
