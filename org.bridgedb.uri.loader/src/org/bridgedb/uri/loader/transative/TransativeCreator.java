4917f735f15c81289d36e2404f896cf4100d5df2
Christian-B <brenninc@cs.man.ac.uk>
2013-10-31 17:36:00 +0000

    Create a transtive mapping set

---
+++ org.bridgedb.uri.loader/src/org/bridgedb/uri/loader/transative/TransativeCreator.java
@@ -0,0 +1,183 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bridgedb.uri.loader.transative;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Set;
+import org.apache.log4j.Logger;
+import org.bridgedb.DataSource;
+import org.bridgedb.rdf.UriPattern;
+import org.bridgedb.sql.SQLAccess;
+import org.bridgedb.sql.SQLUriMapper;
+import org.bridgedb.sql.SqlFactory;
+import org.bridgedb.statistics.DataSetInfo;
+import org.bridgedb.statistics.MappingSetInfo;
+import org.bridgedb.uri.UriMapper;
+import org.bridgedb.utils.BridgeDBException;
+import org.bridgedb.utils.ConfigReader;
+import org.bridgedb.utils.Reporter;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.StatementImpl;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.turtle.TurtleWriter;
+
+/**
+ *
+ * @author Christian
+ */
+public class TransativeCreator {
+
+    private static SQLAccess sqlAccess = null;
+    private final UriMapper mapper;
+    protected final MappingSetInfo leftInfo;
+    protected final MappingSetInfo rightInfo;
+    protected final URI predicate;
+    protected final String justification;
+    private final UriPattern sourceUriPattern;
+    private final UriPattern targetUriPattern;
+    private final boolean reflexive;
+    
+    private static URI GENERATE_PREDICATE = null;
+
+    static final Logger logger = Logger.getLogger(TransativeCreator.class);
+    
+    public static File doTransativeIfPossible(MappingSetInfo left, MappingSetInfo right) throws BridgeDBException, IOException {
+        TransativeCreator creator = new TransativeCreator(left, right);
+        return creator.generateOutputFileIfPossible();
+    }
+
+    public static File doTransativeIfPossible(int leftId, int rightId) 
+            throws BridgeDBException, IOException {
+        SQLUriMapper mapper = SQLUriMapper.getExisting();
+        MappingSetInfo left = mapper.getMappingSetInfo(leftId);
+        MappingSetInfo right = mapper.getMappingSetInfo(rightId);
+        return doTransativeIfPossible(left, right);
+    }
+    
+    protected TransativeCreator(MappingSetInfo left, MappingSetInfo right) 
+            throws BridgeDBException, IOException{
+        if (sqlAccess == null){
+            sqlAccess = SqlFactory.createTheSQLAccess();
+        }
+        mapper = SQLUriMapper.getExisting();
+        leftInfo = left;
+        rightInfo = right;
+        predicate = new URIImpl(PredicateMaker.combine(left.getPredicate(), right.getPredicate()));
+        justification = JustificationMaker.combine(left.getJustification(), right.getJustification());
+        reflexive = left.getSource().getSysCode().equals(right.getTarget().getSysCode());
+        sourceUriPattern = getUriPattern(left.getSource());
+        targetUriPattern = getUriPattern(right.getTarget());
+        checkTransativeLegal(left, right);
+    }
+    
+    public File generateOutputFileIfPossible() throws BridgeDBException, IOException{
+        try {
+            File parent = TransativeConfig.getTransativeDirectory();
+            File outputFile = new File(parent, getid());
+            Reporter.println("Writing transative to " + outputFile.getAbsolutePath());
+            FileWriter writer = new FileWriter(outputFile);
+            RDFWriter rdfWriter = new TurtleWriter(writer);
+            rdfWriter.startRDF();
+            writeHeader(rdfWriter);
+            boolean result = getSQL(rdfWriter);
+            rdfWriter.endRDF();
+            writer.flush();
+            writer.close();
+            if (result){
+                return outputFile;
+            } else {
+                return null;
+            }
+        } catch (RDFHandlerException ex) {
+            throw new BridgeDBException("Error writing to RDF ", ex);
+        }
+    }
+
+    protected String getid(){
+        return "Transitive" + leftInfo.getStringId() + "and" + rightInfo.getStringId() + ".ttl";
+    }
+    
+    private boolean getSQL(RDFWriter rdfwriter) throws BridgeDBException, IOException, RDFHandlerException {
+        boolean found = false;
+         StringBuilder query = new StringBuilder(
+                "SELECT mapping1.sourceId, mapping2.targetId ");
+        query.append("FROM mapping as mapping1, mapping as mapping2 ");
+        query.append("WHERE mapping1.targetId = mapping2.sourceId ");
+        query.append("AND mapping1.mappingSetId = ");
+            query.append(leftInfo.getIntId());
+            query.append(" ");
+        query.append("AND mapping2.mappingSetId = ");
+            query.append(rightInfo.getIntId());
+            query.append(" ");
+        Connection connection = sqlAccess.getConnection();
+        java.sql.Statement statement;
+        try {
+            statement = connection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
+                    java.sql.ResultSet.CONCUR_READ_ONLY);
+            statement.setFetchSize(Integer.MIN_VALUE);
+        } catch (SQLException ex) {
+           throw new BridgeDBException("Unable to get statement. ", ex);
+        }
+        try {
+            logger.info("Running " + query.toString());
+            ResultSet rs = statement.executeQuery(query.toString());
+            logger.info("processing results");
+            while (rs.next()){
+                String sourceId = rs.getString("mapping1.sourceId");
+                String targetId = rs.getString("mapping2.targetId");
+                if (reflexive && sourceId.equals(targetId)){
+                    //do nothing as same uri;
+                } else {
+                    String sourceUri = sourceUriPattern.getPrefix() + sourceId + sourceUriPattern.getPostfix();
+                    URI sourceURI = new URIImpl(sourceUri);
+                    String targetUri = targetUriPattern.getPrefix() + targetId + targetUriPattern.getPostfix();
+                    URI targetURI = new URIImpl(targetUri);
+                    Statement statment = new StatementImpl(sourceURI, predicate, targetURI);
+                    rdfwriter.handleStatement(statment);
+                    found = true;
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new BridgeDBException("Unable to run query. " + query, ex);
+        }
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            throw new BridgeDBException("Error closing MYSQL connection", ex);
+        }
+        return found;
+    }
+    
+    private UriPattern getUriPattern(DataSetInfo info) throws BridgeDBException {
+        DataSource dataSource = info.getDataSource();
+        String pattern = dataSource.getKnownUrl("$id");
+        return UriPattern.existingByPattern(pattern);
+    }
+
+    private void checkTransativeLegal(MappingSetInfo left, MappingSetInfo right) throws BridgeDBException {
+        if (!left.getTarget().equals(right.getSource())){
+            throw new BridgeDBException("Left target " + left.getTarget() + " does not match right source " + right.getSource());
+        }
+    }
+
+    /**
+     * Empty method to allow subclasses to write headers
+     * @param buffer 
+     */
+    protected void writeHeader(RDFWriter writer) throws BridgeDBException, RDFHandlerException {
+        //Do nothing here
+    }
+
+    
+}
---
+++ org.bridgedb.uri.loader/test/org/bridgedb/loader/transative/TransativeCreatorTest.java
@@ -0,0 +1,102 @@
+// BridgeDb,
+// An abstraction layer for identifier mapping services, both local and online.
+//
+// Copyright 2006-2009  BridgeDb developers
+// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+// Copyright 2012-2013  OpenPhacts
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package org.bridgedb.loader.transative;
+
+import java.io.File;
+import org.bridgedb.sql.SQLUriMapper;
+import org.bridgedb.sql.TestSqlFactory;
+import org.bridgedb.statistics.MappingSetInfo;
+import org.bridgedb.uri.Lens;
+import org.bridgedb.uri.loader.LinksetListener;
+import org.bridgedb.uri.loader.RdfParser;
+import org.bridgedb.uri.loader.transative.TransativeCreator;
+import org.bridgedb.utils.BridgeDBException;
+import org.bridgedb.utils.ConfigReader;
+import org.bridgedb.utils.Reporter;
+import static org.hamcrest.Matchers.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+
+/**
+ *
+ * @author Christian
+ */
+public class TransativeCreatorTest {
+    
+    static final URI linkPredicate = new URIImpl("http://www.w3.org/2004/02/skos/core#exactMatch");
+    static SQLUriMapper uriListener;
+    static LinksetListener instance;
+
+    public TransativeCreatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() throws BridgeDBException {
+        TestSqlFactory.checkSQLAccess();
+        ConfigReader.useTest();
+        uriListener = SQLUriMapper.createNew();
+        instance = new LinksetListener(uriListener);
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    protected void loadFile(String fileName, String justification) throws BridgeDBException{
+        File file = new File(fileName);
+        loadFile(file, justification);
+    }
+    
+    protected void loadFile(File file, String justification) throws BridgeDBException{
+        Reporter.println("parsing " + file.getAbsolutePath());
+        int mappingSetId = instance.parse(file, linkPredicate, justification);
+        MappingSetInfo mapping = uriListener.getMappingSetInfo(mappingSetId);
+        int numberOfLinks = mapping.getNumberOfLinks();
+        assertThat(numberOfLinks, greaterThanOrEqualTo(3));      
+    }
+    
+    /**
+     * Test of parse method, of class LinksetListener.
+     */
+    @Test
+    public void testLoadTestData() throws Exception {
+        Reporter.println("LoadTestData");
+        loadFile("../org.bridgedb.uri.loader/test-data/cw-cs.ttl", Lens.getDefaultJustifictaionString());
+        loadFile("../org.bridgedb.uri.loader/test-data/cs-cm.ttl", Lens.getDefaultJustifictaionString());
+        File transative = TransativeCreator.doTransativeIfPossible(1, 3);
+        //assertTrue(transative.exists());
+        //loadFile(transative, MAIN_JUSTIFCATION);
+    }
+
+ }
