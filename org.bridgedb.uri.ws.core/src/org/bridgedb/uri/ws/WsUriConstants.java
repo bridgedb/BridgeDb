eb070dc035acc446fba37f37289b193be812ff07
Christian-B <brenninc@cs.man.ac.uk>
2013-10-31 15:23:31 +0000

    Constants for ws names

---
+++ org.bridgedb.uri.ws.core/src/org/bridgedb/uri/ws/WsUriConstants.java
@@ -0,0 +1,53 @@
+// BridgeDb,
+// An abstraction layer for identifier mapping services, both local and online.
+//
+// Copyright 2006-2009  BridgeDb developers
+// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+// Copyright 2012-2013  OpenPhacts
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package org.bridgedb.uri.ws;
+
+import org.bridgedb.uri.SetMappings;
+import org.bridgedb.ws.WsConstants;
+
+/**
+ *
+ * @author Christian
+ */
+public class WsUriConstants extends WsConstants{
+    
+    
+    public static final String BRIDGEDB_API = "BridgeDbApi";
+    public static final String BRIDGEDB_HOME = "BridgeDb";
+    public static final String DATA_SOURCE = "dataSource";
+    public static final String FORMAT = "format";
+    public static final String GET_OVERALL_STATISTICS = "getOverallStatistics";
+    public static final String GRAPH = "graph";
+    public static final String GRAPHVIZ = "graphviz";
+    public static final String MAP = "map";
+    public static final String MAP_URI = "mapUri";
+    public static final String MAP_BY_SET = "mapBySet";
+    public static final String LENS_URI = "lensUri";
+    public static final String RDF = "RDF";
+    public static final String RDF_FORMAT = "rdfFormat";
+    public static final String SQL_COMPAT_VERSION = "SqlCompatVersion";
+    public static final String TARGET_URI_PATTERN = "targetUriPattern";
+
+    public static final String TO_XREF = "toXref";
+    public static final String URI = "Uri";
+    public static final String URI_EXISTS = "UriExists";
+    public static final String URI_SEARCH = "UriSearch";
+    public static final String XML = "XML";
+}
