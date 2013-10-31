f873d0cbcf58d7ffe93e64751d2a7b0b8db2a5d7
Christian-B <brenninc@cs.man.ac.uk>
2013-10-31 10:30:55 +0000

    Add tests for WS(if it exists) via client Tests will abort if no service exists

---
+++ org.bridgedb.ws.client/test/org/bridgedb/ws/client/IDMapperCapabilitiesTest.java
@@ -0,0 +1,42 @@
+// BridgeDb,
+// An abstraction layer for identifier mapping services, both local and online.
+//
+// Copyright 2006-2009  BridgeDb developers
+// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+// Copyright 2012-2013  OpenPhacts
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package org.bridgedb.ws.client;
+
+import org.bridgedb.utils.BridgeDBException;
+import org.bridgedb.ws.WSCoreClientFactory;
+import org.bridgedb.ws.WSCoreInterface;
+import org.bridgedb.ws.WSCoreMapper;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author Christian
+ */
+@Ignore //repeated in OPS client and depends on the specific BridgeBD.war
+public class IDMapperCapabilitiesTest  extends org.bridgedb.utils.IDMapperCapabilitiesTest{
+    
+    @BeforeClass
+    public static void setupIDMapper() throws BridgeDBException {
+        WSCoreInterface webService = WSCoreClientFactory.createTestWSClient();
+        capabilities = new WSCoreMapper(webService).getCapabilities();
+    }
+
+}
