66acd9d06ef709d9b9dff0b1b85edb64e4edc653
Christian-B <brenninc@cs.man.ac.uk>
2013-10-31 16:38:07 +0000

    Uri predicate constants

---
+++ org.bridgedb.uri.loader/src/org/bridgedb/uri/loader/transative/constant/OboConstants.java
@@ -0,0 +1,11 @@
+package org.bridgedb.uri.loader.transative.constant;
+
+public class OboConstants {
+
+	public static final String PREFIX = "http://purl.obolibrary.org/obo#";
+	
+	public static final String HAS_PART = PREFIX + "has_part";
+	public static final String HAS_FUNCTIONAL_PARENT = PREFIX + "has_functional_parent";
+	public static final String IS_TAUTOMER_OF = PREFIX + "is_tautomer_of";
+;
+}
