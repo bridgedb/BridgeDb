8f24b24a917f1eca35a62691c68361180c421574
Christian-B <brenninc@cs.man.ac.uk>
2013-10-31 08:33:35 +0000

    Added SQL Setup commands

---
+++ mysqlConfig.sql
@@ -0,0 +1,11 @@
+-- MySQL script to create the default user accounts
+
+-- Test database
+CREATE USER 'imstest'@'localhost' identified by 'imstest';
+CREATE DATABASE imstest;
+GRANT CREATE, DROP, DELETE, INDEX, SELECT, INSERT, UPDATE on imstest.* TO 'imstest'@'localhost';
+
+-- Default IMS database
+CREATE USER 'ims'@'localhost' identified by 'ims';
+CREATE DATABASE ims;
+GRANT CREATE, DROP, DELETE, INDEX, SELECT, INSERT, UPDATE, ALTER on ims.* TO 'ims'@'localhost';
