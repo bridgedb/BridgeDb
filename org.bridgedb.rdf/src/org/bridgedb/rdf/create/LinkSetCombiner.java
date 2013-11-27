8dd1ded4dcd06aa86633525374a8a494276ce419
Christian-B <brenninc@cs.man.ac.uk>
2013-11-27 15:52:23 +0000

    Actual code for linkset combiner

--- org.bridgedb.rdf/src/org/bridgedb/rdf/create/LinkSetCombiner.java
+++ org.bridgedb.rdf/src/org/bridgedb/rdf/create/LinkSetCombiner.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Collection;
+import java.util.HashSet;
 import org.bridgedb.rdf.constants.BridgeDBConstants;
 import org.bridgedb.utils.BridgeDBException;
 import org.bridgedb.utils.Reporter;
@@ -30,7 +32,7 @@ import org.openrdf.sail.memory.MemoryStore;
  *
  * @author Christian
  */
-public class FormatConvertor {
+public class LinkSetCombiner {
     
    public static String DEFAULT_BASE_URI = "http://no/BaseURI/Set/";
    
@@ -70,15 +72,17 @@ public class FormatConvertor {
         rdfWriter.endRDF();
     }
     
-   public static void convert(File inputFile, File outputFile) throws Exception {
-        Reporter.println("Parsing " + inputFile.getAbsolutePath());
+   public static void convert(Collection<File> inputFiles, File outputFile) throws Exception {
         Repository repository = null;
         RepositoryConnection repositoryConnection = null;
         try {
             repository = new SailRepository(new MemoryStore());
             repository.initialize();
             repositoryConnection = repository.getConnection();
-            repositoryConnection.add(inputFile, DEFAULT_BASE_URI, getFormat(inputFile.getName()));
+            for (File inputFile:inputFiles){
+                Reporter.println("Parsing " + inputFile.getAbsolutePath());
+                repositoryConnection.add(inputFile, DEFAULT_BASE_URI, getFormat(inputFile.getName()));
+            }
             writeRDF(repositoryConnection, outputFile);
         } catch (Exception ex) {
             throw new BridgeDBException ("Error parsing RDf file ", ex);
@@ -87,9 +91,49 @@ public class FormatConvertor {
         }
     }
 
+   public static long count(File inputFile) throws Exception {
+        Repository repository = null;
+        RepositoryConnection repositoryConnection = null;
+        try {
+            repository = new SailRepository(new MemoryStore());
+            repository.initialize();
+            repositoryConnection = repository.getConnection();
+            Reporter.println("Parsing (to count)" + inputFile.getAbsolutePath());
+            repositoryConnection.add(inputFile, DEFAULT_BASE_URI, getFormat(inputFile.getName()));
+            long result = repositoryConnection.size();
+            System.out.println("Count is " + result);
+            return result;
+        } catch (Exception ex) {
+            throw new BridgeDBException ("Error parsing RDf file ", ex);
+        } finally {
+            repositoryConnection.close();
+        }
+   }
+   
     public static void main(String[] args) throws Exception {
-        convert(new File("C:/Temp/biomodels_void-dcat.rdf"), new File("C:/Temp/biomodels_void-dcat.ttl"));
-        convert(new File("C:/Temp/biomodels_void-ops.rdf"), new File("C:/Temp/biomodels_void-ops.ttl"));
-        convert(new File("C:/Temp/example_void-id.org.rdf"), new File("C:/Temp/example_void-id.org.ttl"));
+        long total = 0;
+        HashSet<File> files = new HashSet<File>();
+        File f1 = new File("C:/Dropbox/ims/linkset/version1.3/CRS/CHEBI/LINKSET_EXACT_OPS_CHEMSPIDER_CHEBI20131111.ttl");
+        total+= count(f1);
+        files.add(f1);
+        File f2 = new File("C:/Dropbox/ims/linkset/version1.3/CRS/CHEMBL/LINKSET_EXACT_OPS_CHEMSPIDER_CHEMBL20131111.ttl");
+        total+=  count(f2);
+        files.add(f2);
+        File f3 = new File("C:/Dropbox/ims/linkset/version1.3/CRS/DRUGBANK/LINKSET_EXACT_OPS_CHEMSPIDER_DRUGBANK20131111.ttl");
+        total+=  count(f3);
+        files.add(f3);
+        File f4 = new File("C:/Dropbox/ims/linkset/version1.3/CRS/HMDB/LINKSET_EXACT_OPS_CHEMSPIDER_HMDB20131111.ttl");
+        total+=  count(f4);
+        files.add(f4);
+        File f5 = new File("C:/Dropbox/ims/linkset/version1.3/CRS/MESH/LINKSET_EXACT_OPS_CHEMSPIDER_MESH20131111.ttl");
+        total+=  count(f5);
+        files.add(f5);
+        File f6 = new File("C:/Dropbox/ims/linkset/version1.3/CRS/PDB/LINKSET_EXACT_OPS_CHEMSPIDER_PDB20131111.ttl");
+        total+=  count(f6);
+        files.add(f6);
+        File fresult = new File("C:/Temp/CRS_combined.ttl");
+        convert(files, fresult);
+        long combined = count(fresult);
+        System.out.println("Detected " + (combined - total) + " duplicates");
     }
 }
