4740dad69c9558b242e7e23a1703f529ba986b42
Christian-B <brenninc@cs.man.ac.uk>
2014-01-20 11:33:04 +0100

    Added update permission needed for mysqldump

--- mysqlConfig.sql
+++ mysqlConfig.sql
@@ -3,9 +3,9 @@
 -- Test database
 CREATE USER 'imstest'@'localhost' identified by 'imstest';
 CREATE DATABASE imstest;
-GRANT CREATE, DROP, DELETE, INDEX, SELECT, INSERT, UPDATE on imstest.* TO 'imstest'@'localhost';
+GRANT ALTER, CREATE, DROP, DELETE, INDEX, SELECT, INSERT, UPDATE on imstest.* TO 'imstest'@'localhost';
 
 -- Default IMS database
 CREATE USER 'ims'@'localhost' identified by 'ims';
 CREATE DATABASE ims;
-GRANT CREATE, DROP, DELETE, INDEX, SELECT, INSERT, UPDATE, ALTER on ims.* TO 'ims'@'localhost';
+GRANT ALTER, CREATE, DROP, DELETE, INDEX, SELECT, INSERT, UPDATE, ALTER on ims.* TO 'ims'@'localhost';
