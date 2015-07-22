73388736e124a41fb0f563e37399ef1dd67b8769
Egon Willighagen <egon.willighagen@gmail.com>
2015-07-22 18:42:13 +0200

    Now only shows the removed IDs when you run it with '-DshowRemovedIDs=true'

--- org.bridgedb.tools.qc/src/org/bridgedb/tools/qc/BridgeQC.java
+++ org.bridgedb.tools.qc/src/org/bridgedb/tools/qc/BridgeQC.java
@@ -121,7 +121,7 @@ public class BridgeQC
 					genesRemoved.size(),
 					(delta * 100)
 				);
-				if (genesRemoved.size() > 0)
+				if (genesRemoved.size() > 0 && "true".equals(System.getProperty("showRemovedIDs", "false")))
 					System.out.printf(
 						"INFO: The ids removed from %s%s: %s\n",
 							ds.getSystemCode(),
