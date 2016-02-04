3295ba9ca727f3af4bde10aff4038076814ebb53
Egon Willighagen <egon.willighagen@gmail.com>
2016-02-04 07:32:39 +0100

    Use the Maven-generated jars

--- qc.sh
+++ qc.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-java -DstrictDataSourceChecking=false -jar dist/org.bridgedb.tools.qc.jar "$@"
+#
+# Run after first compiling with "mvn compile install" and updating the version
+# number below.
+
+VERSION=2.2.0-SNAPSHOT
+CLASSPATH=org.bridgedb.tools.qc/target/org.bridgedb.tools.qc-${VERSION}.jar:\
+org.bridgedb/target/org.bridgedb-${VERSION}.jar:\
+org.bridgedb.bio/target/org.bridgedb.bio-${VERSION}.jar:\
+org.bridgedb.rdb/target/org.bridgedb.rdb-${VERSION}.jar:\
+org.bridgedb.rdb.construct/target/org.bridgedb.rdb.construct-${VERSION}.jar:\
+${HOME}/.m2/repository/org/apache/derby/derby/10.5.3.0_1/derby-10.5.3.0_1.jar:\
+${HOME}/.m2/repository/com/google/collections/google-collections/1.0/google-collections-1.0.jar
+
+java -DstrictDataSourceChecking=false -cp ${CLASSPATH} \
+  org.bridgedb.tools.qc.BridgeQC "$@"
