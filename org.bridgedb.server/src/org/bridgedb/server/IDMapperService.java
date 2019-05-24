691b7ce10de1879120df4c33fd7a49b041475e71
nunogit <nunomsilvanunes@gmail.com>
2019-05-24 10:50:05 +0200

    fix to license

--- org.bridgedb.server/src/org/bridgedb/server/IDMapperService.java
+++ org.bridgedb.server/src/org/bridgedb/server/IDMapperService.java
@@ -299,6 +299,10 @@ public class IDMapperService extends Application {
                 //Register the route for a url pattern that doesn't match other patterns
                 router.attach(URL_NO_MATCH, NoMatch.class);
                 
+		String xrefs = "/batch/{" + PAR_ORGANISM + "}/xrefs/{" + PAR_SYSTEM + "}";
+		router.attach("/batch/{" + PAR_ORGANISM + "}/xrefs",Batch.class);
+		Route batchRoute = router.attach(xrefs,Batch.class);
+		batchRoute.extractQuery(PAR_TARGET_SYSTEM, PAR_TARGET_SYSTEM, true);
 		return router;
 	}
 
