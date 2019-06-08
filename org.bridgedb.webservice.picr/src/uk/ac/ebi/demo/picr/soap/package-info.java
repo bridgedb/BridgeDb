0d4da8688a240194998c5d36527d4401856022e8
Egon Willighagen <egon.willighagen@gmail.com>
2019-06-08 14:03:42 +0200

    Removed three further defunct webservice wrappers

--- org.bridgedb.webservice.picr/src/uk/ac/ebi/demo/picr/soap/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ebi.ac.uk/picr/AccessionMappingService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package uk.ac.ebi.demo.picr.soap;
