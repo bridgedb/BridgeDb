1ab9dc6778c4cf8d6bf502f7f82b7e6dbcde6df1
Manas Awasthi <marvex17@gmail.com>
2019-06-10 17:01:03 +0530

    Update OrganismTest.java
    updated JUnit4 to JUnit5

--- org.bridgedb.bio/test/org/bridgedb/bio/OrganismTest.java
+++ org.bridgedb.bio/test/org/bridgedb/bio/OrganismTest.java
@@ -1,106 +1,104 @@
-// BridgeDb,
-// An abstraction layer for identifier mapping services, both local and online.
-// Copyright 2006-2009 BridgeDb developers
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/*
+ *BridgeDb,
+ *An abstraction layer for identifier mapping services, both local and online.
+ *Copyright (c) 2006-2009 BridgeDb Developers
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package org.bridgedb.bio;
 
 import java.util.HashSet;
 import java.util.Set;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrganismTest
 {
 
-    @Before
+    @BeforeEach
         public void setUp() {
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void tasteWine() {
         Organism wine = Organism.VitisVinifera;
         // VitisVinifera ("Vitis vinifera", "Vv", "Wine Grape"),
-        Assert.assertEquals("Vitis vinifera", wine.latinName());
-        Assert.assertEquals("Vv", wine.code());
-        Assert.assertEquals("Wine Grape", wine.shortName());
+        assertEquals("Vitis vinifera", wine.latinName());
+        assertEquals("Vv", wine.code());
+        assertEquals("Wine Grape", wine.shortName());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testFromCode() {
         Organism org = Organism.fromCode("Zm");
-        Assert.assertNotNull(org);
+        assertNotNull(org);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testFromCodeNull() {
         Organism org = Organism.fromCode("Null");
-        Assert.assertNull(org);
+        assertNull(org);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testFromShortName() {
         Organism org = Organism.fromShortName("Frog");
-        Assert.assertNotNull(org);
+        assertNotNull(org);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testFromTaxonomyID() {
         Organism org = Organism.fromTaxonomyId(9606);
-        Assert.assertNotNull(org);
-        Assert.assertEquals("Homo sapiens", org.latinName());
+        assertNotNull(org);
+        assertEquals("Homo sapiens", org.latinName());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testFromNullShortName() {
         Organism org = Organism.fromShortName("Null");
-        Assert.assertNull(org);
+        assertNull(org);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testCodes() {
         String[] codes = Organism.codes();
-        Assert.assertNotNull(codes);
-        Assert.assertNotSame(0, codes.length);
+        assertNotNull(codes);
+        assertNotSame(0, codes.length);
     }
 
-   @Test
-   public void testUniqueLatinNames() {
+    @org.junit.jupiter.api.Test
+    public void testUniqueLatinNames() {
 	   Organism[] organisms = Organism.values();
 	   Set<String> latinNames = new HashSet<String>();
 	   for (Organism organism : organisms) {
 		   latinNames.add(organism.latinName());
 	   }
-	   Assert.assertEquals(organisms.length, latinNames.size());
+	   assertEquals(organisms.length, latinNames.size());
    }
 
-   @Test
-   public void testUniqueCodes() {
+    @org.junit.jupiter.api.Test
+    public void testUniqueCodes() {
 	   Organism[] organisms = Organism.values();
 	   Set<String> codes = new HashSet<String>();
 	   for (Organism organism : organisms) {
 		   codes.add(organism.code());
 	   }
-	   Assert.assertEquals(organisms.length, codes.size());
+	   assertEquals(organisms.length, codes.size());
    }
 
-   @Test
-   public void tasteHuman() {
+    @org.junit.jupiter.api.Test
+    public void tasteHuman() {
        Organism wine = Organism.HomoSapiens;
-       Assert.assertEquals("Homo sapiens", wine.latinName());
-       Assert.assertEquals("9606", wine.taxonomyID().getId());
+       assertEquals("Homo sapiens", wine.latinName());
+       assertEquals("9606", wine.taxonomyID().getId());
    }
 }
