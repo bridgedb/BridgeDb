e60ea731cf9ef38b6ddf53b5b08572bed323b411
Manas Awasthi <marvex17@gmail.com>
2019-06-10 20:55:36 +0530

    Update IDMapperTest.java
    updated JUnit4 to JUnit5

--- org.bridgedb/test/org/bridgedb/IDMapperTest.java
+++ org.bridgedb/test/org/bridgedb/IDMapperTest.java
@@ -1,29 +1,28 @@
-// BridgeDb,
-// An abstraction layer for identifier mapping services, both local and online.
-//
-// Copyright      2012  Christian Y. A. Brenninkmeijer
-// Copyright      2012  OpenPhacts
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/*
+ *BridgeDb,
+ *An abstraction layer for identifier mapping services, both local and online.
+ *Copyright (c) 2012  Christian Y. A. Brenninkmeijer
+ *Copyright (c) 2012 OpenPhacts
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package org.bridgedb;
 
-import org.junit.AfterClass;
 import java.util.Map;
 import java.util.HashSet;
 import java.util.Set;
-import org.junit.Ignore;
-import org.junit.Test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+
 import static org.junit.Assert.*;
 
 /**
@@ -36,7 +35,7 @@ import static org.junit.Assert.*;
  *
  * @author Christian
  */
-@Ignore
+@Disabled
 public abstract class IDMapperTest extends IDMapperCapabilitiesTest{
             
     static protected IDMapper idMapper;
@@ -45,7 +44,7 @@ public abstract class IDMapperTest extends IDMapperCapabilitiesTest{
     //If set to false will skip testClose(). 
     protected static boolean connectionOk = true;
     
-    @AfterClass //Setup as an afterclass so it is run last.
+    @AfterAll //Setup as an afterclass so it is run last.
     public static void testClose() throws IDMapperException{
         if (connectionOk){        
             assertTrue (idMapper.isConnected());
@@ -189,7 +188,7 @@ public abstract class IDMapperTest extends IDMapperCapabilitiesTest{
     }
 
     @Test
-    @Ignore // There is no longer an id in the test data used more than once.
+    @Disabled // There is no longer an id in the test data used more than once.
     public void testFreeSearchGoodJust2() throws IDMapperException{
         org.junit.Assume.assumeTrue(idMapper.getCapabilities().isFreeSearchSupported());       
         report("FreeSearchGoodJust2");
