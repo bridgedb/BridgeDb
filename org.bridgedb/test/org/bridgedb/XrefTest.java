f434ffb5cdbd1580038b8bb887eb45416958c92e
Manas Awasthi <marvex17@gmail.com>
2019-06-10 20:48:36 +0530

    Update XrefTest.java
    updated JUnit4 to JUnit5

--- org.bridgedb/test/org/bridgedb/XrefTest.java
+++ org.bridgedb/test/org/bridgedb/XrefTest.java
@@ -1,21 +1,21 @@
-// Copyright      2013  Egon Willighagen <egonw@users.sf.net>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/*
+ *BridgeDb,
+ *An abstraction layer for identifier mapping services, both local and online.
+ *Copyright (c) 2013 Egon Willighagen <egonw@users.sf.net>
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and limitations under the License.
+ */
 package org.bridgedb;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class XrefTest {
 
@@ -27,64 +27,64 @@ public class XrefTest {
 	@Test
 	public void testConstructor() {
 		Xref xref = new Xref("ENSG000001", EN);
-		Assert.assertNotNull(xref);
-		Assert.assertEquals("ENSG000001", xref.getId());
-		Assert.assertNotNull(xref.getDataSource());
-		Assert.assertEquals("En", xref.getDataSource().getSystemCode());
+		assertNotNull(xref);
+		assertEquals("ENSG000001", xref.getId());
+		assertNotNull(xref.getDataSource());
+		assertEquals("En", xref.getDataSource().getSystemCode());
 	}
 
 	@Test
 	public void testEquals_Null() {
 		Xref xref = new Xref("ENSG000001", EN);
-		Assert.assertFalse(xref.equals(null));
+		assertNotEquals(null, xref);
 	}
 
 	@Test
 	public void testEquals_NonXref() {
 		Xref xref = new Xref("ENSG000001", EN);
-		Assert.assertFalse(xref.equals("ENSG000001"));
+		assertNotEquals("ENSG000001", xref);
 	}
 
 	@Test
 	public void testEquals_DiffId() {
 		Xref xref = new Xref("ENSG000001", EN);
 		Xref xref2 = new Xref("ENSG000002", EN);
-		Assert.assertFalse(xref.equals(xref2));
-		Assert.assertFalse(xref2.equals(xref)); // and symmetric
+		assertNotEquals(xref, xref2);
+		assertNotEquals(xref2, xref); // and symmetric
 	}
 
 	@Test
 	public void testEquals() {
 		Xref xref = new Xref("ENSG000001", EN);
 		Xref xref2 = new Xref("ENSG000001", EN);
-		Assert.assertTrue(xref.equals(xref2));
-		Assert.assertTrue(xref2.equals(xref)); // and symmetric
+		assertEquals(xref, xref2);
+		assertEquals(xref2, xref); // and symmetric
 	}
 
 	@Test
 	public void testCompareTo() {
 		Xref xref = new Xref("ENSG000001", EN);
 		Xref xref2 = new Xref("ENSG000001", EN);
-		Assert.assertEquals(0, xref.compareTo(xref2));
-		Assert.assertEquals(0, xref2.compareTo(xref)); // and symmetric
+		assertEquals(0, xref.compareTo(xref2));
+		assertEquals(0, xref2.compareTo(xref)); // and symmetric
 	}
 
 	@Test
 	public void testCompareTo_Diff() {
 		Xref xref = new Xref("ENSG000001", EN);
 		Xref xref2 = new Xref("ENSG000002", EN);
-		Assert.assertNotSame(0, xref.compareTo(xref2));
-		Assert.assertNotSame(0, xref2.compareTo(xref)); // and symmetric
+		assertNotSame(0, xref.compareTo(xref2));
+		assertNotSame(0, xref2.compareTo(xref)); // and symmetric
 	}
 
 	@Test
 	public void testURNRoundtripping() {
 		Xref xref = new Xref("P12345", UNIPROT);
 		String urn = xref.getURN();
-		Assert.assertNotNull(urn);
-		Assert.assertNotSame(0, urn.length());
+		assertNotNull(urn);
+		assertNotSame(0, urn.length());
 		Xref xref2 = Xref.fromUrn(urn);
-		Assert.assertTrue(xref.equals(xref2));
-		Assert.assertTrue(xref2.equals(xref)); // and symmetric
+		assertEquals(xref, xref2);
+		assertEquals(xref2, xref); // and symmetric
 	}
 }
