7af171f3cf32eaf447a849f09b0b49d904b3f025
Manas Awasthi <marvex17@gmail.com>
2019-06-10 20:30:38 +0530

    Update IDMapperStackTest.java
    updated JUnit4 to JUnit5

--- org.bridgedb/test/org/bridgedb/IDMapperStackTest.java
+++ org.bridgedb/test/org/bridgedb/IDMapperStackTest.java
@@ -1,20 +1,22 @@
-// BridgeDb,
-// An abstraction layer for identifier mapping services, both local and online.
-// Copyright 2006-2013 BridgeDb developers
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ *BridgeDb,
+ *An abstraction layer for identifier mapping services, both local and online.
+ *Copyright (c) 2006 - 2013 BridgeDb Developers
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and limitations under the License.
+ */
+
 //
 package org.bridgedb;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -22,16 +24,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class IDMapperStackTest extends TestCase 
+
+public class IDMapperStackTest
 {
 	public static Map<String, IDMapper> mappers;
 	public static final String FILENAMES [] = { "AB", "BC", "CD", "DE", "XY", "XZ", "YW", "YZ" };
 	private static IDMapperStack stack;
 	
 	private static DataSource dsW, dsX, dsY, dsZ, dsA, dsE, dsB, dsC, dsD;
-	
+
+
+	@org.junit.jupiter.api.Test
 	protected void setUp() throws ClassNotFoundException, IDMapperException, MalformedURLException
 	{
 		Class.forName("org.bridgedb.file.IDMapperText");
@@ -45,7 +50,7 @@ public class IDMapperStackTest extends TestCase
 			String fullName = fileName + ".csv";
 			ClassLoader classLoader = this.getClass().getClassLoader();
 			URL url = classLoader.getResource(fullName);
-			assertNotNull("Could not find resource in classpath: " + fullName, url);
+			assertNotNull(url, "Could not find resource in classpath: " + fullName);
 			IDMapper m = BridgeDb.connect("idmapper-text:" + url);
 			mappers.put(fileName, m);
 			stack.addIDMapper(m);
@@ -61,7 +66,8 @@ public class IDMapperStackTest extends TestCase
 		dsD = DataSource.getByFullName("D");
 		dsE = DataSource.getByFullName("E");
 	}
-		
+
+	@Test
 	public void testMapIDXrefDataSourceArray() throws IDMapperException {
 				
 		Xref src = new Xref ("e1", dsE );
@@ -74,43 +80,51 @@ public class IDMapperStackTest extends TestCase
 		assertTrue (results.contains (new Xref("d1", dsD)));
 		assertTrue(true);
 	}
-	
-	public void testSimpleMapID() throws IDMapperException{
-		
-		Xref src = new Xref ("x1", dsX );
-		Set<Xref> results = stack.getIDMapperAt(0).mapID( src, dsY );
-		System.out.println("single IDMapper");
-		System.out.println("src Xref: " + src);
-		System.out.println("src.dataSource: " + src.getDataSource());
-		System.out.println("results.size(): " + results.size());
-		for( Xref x : results ) {
-			System.out.println(x);
-		}
+
+	@Test
+	public void testSimpleMapID() throws IDMapperException {
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Xref src = new Xref ("x1", dsX );
+			Set<Xref> results = stack.getIDMapperAt(0).mapID( src, dsY );
+			System.out.println("single IDMapper");
+			System.out.println("src Xref: " + src);
+			System.out.println("src.dataSource: " + src.getDataSource());
+			System.out.println("results.size(): " + results.size());
+			for( Xref x : results ) {
+				System.out.println(x);
+			}
+		});
 	}
-	
+
+	@Test
 	public void testMapID_A_to_E () throws IDMapperException 
-	{		
-		Xref src = new Xref ("a1", dsA );
-		Set<Xref> results = stack.mapID( src, dsE );		
-		System.out.println("results.size(): " + results.size());
-		for( Xref x : results ) {
-			System.out.println(x);
-		}
-		assertEquals (1, results.size());
-		assertTrue (results.contains (new Xref("e1", dsE )));
+	{
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Xref src = new Xref ("a1", dsA );
+			Set<Xref> results = stack.mapID( src, dsE );
+			System.out.println("results.size(): " + results.size());
+			for( Xref x : results ) {
+				System.out.println(x);
+			}
+			assertEquals (1, results.size());
+			assertTrue (results.contains (new Xref("e1", dsE )));
+		});
 	}
-	
+
+	@Test
 	public void testMapID_X_W_via_Y () throws IDMapperException 
 	{
-		Xref src = new Xref ("x2", dsX );
-		Set<Xref> results = stack.mapID( src, dsW );		
-		assertEquals (1, results.size());
-		assertTrue (results.contains (new Xref("w2", dsW )));
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Xref src = new Xref ("x2", dsX );
+			Set<Xref> results = stack.mapID( src, dsW );
+			assertEquals (1, results.size());
+			assertTrue (results.contains (new Xref("w2", dsW )));
+		});
 	}
-	
+
+	@Test
 	/** do an untargetted mapping */
-	public void testMapID_all () throws IDMapperException
-	{
+	public void testMapID_all () throws IDMapperException {
 		Xref src = new Xref ("x2", dsX );
 		Set<Xref> results = stack.mapID( src );
 		System.out.println ("RESULTS");
@@ -121,5 +135,4 @@ public class IDMapperStackTest extends TestCase
 		assertTrue (results.contains (new Xref("z2", dsZ )));
 		assertTrue (results.contains (new Xref("w2", dsW )));
 	}
-
 }
