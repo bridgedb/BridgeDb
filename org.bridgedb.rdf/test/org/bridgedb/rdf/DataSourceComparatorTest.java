2ad056fa5e6e2b6a1d65eb942357c7d6cdf9c423
Manas Awasthi <marvex17@gmail.com>
2019-06-11 10:09:25 +0530

    Update DataSourceComparatorTest.java
    Updated JUnit4 to JUnit5

--- org.bridgedb.rdf/test/org/bridgedb/rdf/DataSourceComparatorTest.java
+++ org.bridgedb.rdf/test/org/bridgedb/rdf/DataSourceComparatorTest.java
@@ -1,39 +1,32 @@
-// BridgeDb,
-// An abstraction layer for identifier mapping services, both local and online.
-//
-// Copyright 2006-2009  BridgeDb developers
-// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
-// Copyright 2012-2013  OpenPhacts
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/*
+ * BridgeDb,
+ * An abstraction layer for identifier mapping services, both local and online.
+ * Copyright (c) 2006 - 2009  BridgeDb Developers
+ * Copyright (c) 2012-2013 Christian Y. A. Brenninkmeiier
+ * Copyright (c) 2012 - 2013 OpenPhacts
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package org.bridgedb.rdf;
 
-import java.io.File;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Set;
 import org.bridgedb.DataSource;
 import org.bridgedb.bio.DataSourceComparator;
 import org.bridgedb.utils.BridgeDBException;
 import org.bridgedb.utils.Reporter;
 import static org.hamcrest.Matchers.*;
-import org.junit.After;
-import org.junit.AfterClass;
+
 import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+
 import org.openrdf.model.Resource;
 import org.openrdf.model.impl.URIImpl;
 
@@ -49,19 +42,19 @@ public class DataSourceComparatorTest {
 	public DataSourceComparatorTest() {
     }
     
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
     }
     
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() {
     }
     
-    @Before
+    @BeforeEach
     public void setUp() {
     }
     
-    @After
+    @AfterEach
     public void tearDown() {
     }
 
