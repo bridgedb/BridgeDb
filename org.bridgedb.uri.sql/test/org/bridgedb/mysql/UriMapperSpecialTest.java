6ec3e752e3f0bdd1d5bdba8cc19afd5f4c2dd3a3
Manas Awasthi <marvex17@gmail.com>
2019-06-11 17:00:21 +0530

    Update UriMapperSpecialTest.java
    Update JUnit4 to JUnit5

--- org.bridgedb.uri.sql/test/org/bridgedb/mysql/UriMapperSpecialTest.java
+++ org.bridgedb.uri.sql/test/org/bridgedb/mysql/UriMapperSpecialTest.java
@@ -1,22 +1,24 @@
-// BridgeDb,
-// An abstraction layer for identifier mapping services, both local and online.
-//
-// Copyright 2006-2009  BridgeDb developers
-// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
-// Copyright 2012-2013  OpenPhacts
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/*
+ * BridgeDb,
+ * An abstraction layer for identifier mapping services, both local and online.
+ *
+ * Copyright 2006-2009  BridgeDb developers
+ * Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+ * Copyright 2012-2013  OpenPhacts
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.bridgedb.mysql;
 
 import java.util.Date;
@@ -24,8 +26,8 @@ import org.bridgedb.sql.SQLUriMapper;
 import org.bridgedb.sql.TestSqlFactory;
 import org.bridgedb.utils.BridgeDBException;
 import org.bridgedb.utils.ConfigReader;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
+
+import org.junit.jupiter.api.BeforeAll;
 
 /**
  * Runs the UriMapper interface tests over SQLUriMapper class
@@ -40,7 +42,7 @@ public class UriMapperSpecialTest extends org.bridgedb.uri.UriMapperSpecialTest 
     private static final String PREDICATE1 = "testMapping";
     private static final long CREATION1 = new Date().getTime();
 
-    @BeforeClass
+    @BeforeAll
     public static void setupIDMapper() throws BridgeDBException{
 
         connectionOk = false;
