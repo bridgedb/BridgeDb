ef62245f0e75f9ed40716d18f97d50d2b54f5175
Manas Awasthi <marvex17@gmail.com>
2019-06-11 15:18:34 +0530

    Update UriPatternTest.java
    Updated JUnit4 to JUnit5

--- org.bridgedb.rdf/test/org/bridgedb/rdf/UriPatternTest.java
+++ org.bridgedb.rdf/test/org/bridgedb/rdf/UriPatternTest.java
@@ -1,34 +1,30 @@
-// BridgeDb,
-// An abstraction layer for identifier mapping services, both local and online.
-//
-// Copyright 2006-2009  BridgeDb developers
-// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
-// Copyright 2012-2013  OpenPhacts
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/*
+ * BridgeDb,
+ * An abstraction layer for identifier mapping services, both local and online.
+ * Copyright (c) 2006 - 2009  BridgeDb Developers
+ * Copyright (c) 2012-2013 Christian Y. A. Brenninkmeijer
+ * Copyright (c) 2012 - 2013 OpenPhacts
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package org.bridgedb.rdf;
 
 import org.bridgedb.utils.BridgeDBException;
 import org.bridgedb.utils.Reporter;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.number.OrderingComparison.*;
-import org.junit.After;
-import org.junit.AfterClass;
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import org.junit.jupiter.api.*;
 
 /**
  *
@@ -39,23 +35,23 @@ public class UriPatternTest {
     public UriPatternTest() {
     }
     
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
     }
     
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() {
     }
     
-    @Before
+    @BeforeEach
     public void setUp() {
     }
     
-    @After
+    @AfterEach
     public void tearDown() {
     }
 
-    @Ignore("Cannot make sense of this fail: the $id gets added, but the map is empty")
+    @Disabled("Cannot make sense of this fail: the $id gets added, but the map is empty")
     public void testByPatternNoId() throws BridgeDBException{
         Reporter.println("ByPatternNoId");
         UriPattern result = UriPattern.byPattern("http://rdf.ebi.ac.uk/resource/chembl/molecule/");
@@ -65,7 +61,7 @@ public class UriPatternTest {
     /**
      * Test of compareTo method, of class UriPattern.
      */
-    @Test 
+    @Test
     public void testCompareTo() throws BridgeDBException {
         Reporter.println("compareTo");
         UriPattern pattern1 = UriPattern.register("http://www.example.com/UriPatternTest/testCompareTo/1$id", "UriPatternTest", UriPatternType.dataSourceUriPattern);
