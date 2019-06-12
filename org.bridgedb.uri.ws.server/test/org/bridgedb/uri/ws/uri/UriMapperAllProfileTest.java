72cbd8164c1e39178720cba93a34e7d7a56714ac
Manas Awasthi <marvex17@gmail.com>
2019-06-12 16:01:29 +0530

    Update UriMapperAllProfileTest.java
    Update JUnit4 to JUnit5

--- org.bridgedb.uri.ws.server/test/org/bridgedb/uri/ws/uri/UriMapperAllProfileTest.java
+++ org.bridgedb.uri.ws.server/test/org/bridgedb/uri/ws/uri/UriMapperAllProfileTest.java
@@ -1,22 +1,22 @@
-// BridgeDb,
-// An abstraction layer for identifier mapping services, both local and online.
-//
-// Copyright 2006-2009  BridgeDb developers
-// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
-// Copyright 2012-2013  OpenPhacts
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/* BridgeDb,
+ * An abstraction layer for identifier mapping services, both local and online.
+ *
+ * Copyright 2006-2009  BridgeDb developers
+ * Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+ * Copyright 2012-2013  OpenPhacts
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.bridgedb.uri.ws.uri;
 
 import java.util.Date;
@@ -26,20 +26,21 @@ import org.bridgedb.uri.ws.WSUriMapper;
 import org.bridgedb.utils.BridgeDBException;
 import org.bridgedb.utils.ConfigReader;
 import org.bridgedb.ws.uri.WSUriInterfaceService;
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+
 
 /**
  * 
  * @author Christian
  */
-//@Ignore
+//@Disable
 public class UriMapperAllProfileTest extends org.bridgedb.uri.UriMapperAllLensTest {
 
     private static final String CREATOR1 = "testCreator";
     private static final String PREDICATE1 = "testMapping";
     private static final long CREATION1 = new Date().getTime();
 
-    @BeforeClass
+    @BeforeAll
     public static void setupIDMapper() throws BridgeDBException{
         connectionOk = false;
         TestSqlFactory.checkSQLAccess();
