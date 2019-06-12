cdbf026c3ec369fb764b569d1ae26bf201d983f8
Manas Awasthi <marvex17@gmail.com>
2019-06-12 13:53:53 +0530

    Update UriMapperRdfTest.java
    Update JUnit4 to JUnit5

--- org.bridgedb.uri.sql/test/org/bridgedb/uri/UriMapperRdfTest.java
+++ org.bridgedb.uri.sql/test/org/bridgedb/uri/UriMapperRdfTest.java
@@ -1,26 +1,23 @@
-// BridgeDb,
-// An abstraction layer for identifier mapping services, both local and online.
-//
-// Copyright 2006-2009  BridgeDb developers
-// Copyright 2012-2013  Christian Y. A. Brenninkmeijer
-// Copyright 2012-2013  OpenPhacts
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+/* BridgeDb,
+* An abstraction layer for identifier mapping services, both local and online.
+*
+* Copyright 2006-2009  BridgeDb developers
+* Copyright 2012-2013  Christian Y. A. Brenninkmeijer
+* Copyright 2012-2013  OpenPhacts
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.bridgedb.uri;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
 
@@ -29,7 +26,7 @@ import org.bridgedb.sql.transative.DirectMapping;
 import org.bridgedb.statistics.MappingSetInfo;
 import org.bridgedb.uri.api.Mapping;
 import org.bridgedb.uri.tools.StatementMaker;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 import org.openrdf.model.Model;
 import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
@@ -38,6 +35,9 @@ import org.openrdf.model.impl.URIImpl;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.Rio;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests the UriMapper interface (and by loading the UriListener interface)
