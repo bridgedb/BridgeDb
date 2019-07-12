54326bcb24a67662236c528fad1989cb979cb3ba
Manas Awasthi <marvex17@gmail.com>
2019-07-13 02:32:48 +0530

    added "mysql" tag for JUnit Tests

--- org.bridgedb.uri.sql/test/org/bridgedb/mysql/UriMapperNullTargetTest.java
+++ org.bridgedb.uri.sql/test/org/bridgedb/mysql/UriMapperNullTargetTest.java
@@ -42,6 +42,7 @@ public class UriMapperNullTargetTest extends org.bridgedb.uri.UriMapperNullTarge
     private static final long CREATION1 = new Date().getTime();
 
     @BeforeAll
+    @Tag("mysql")
     public static void setupIDMapper() throws BridgeDBException{
 
         connectionOk = false;
