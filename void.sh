8abd1b70949487b563aa1e161d08ef197b9fc855
Manas Awasthi <marvex17@gmail.com>
2019-07-21 02:49:25 +0530

    Create void.sh

---
+++ void.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Run after first compiling with "mvn compile install" and updating the version
+# number below.
+
+VERSION=`tail -1 org.bridgedb/version.props | cut -d'=' -f2`
+CLASSPATH=org.bridgedb.tools.qc/target/org.bridgedb.tools.qc-${VERSION}.jar:\
+org.bridgedb/target/org.bridgedb-${VERSION}.jar:\
+org.bridgedb.bio/target/org.bridgedb.bio-${VERSION}.jar:\
+org.bridgedb.rdb/target/org.bridgedb.rdb-${VERSION}.jar:\
+org.bridgedb.rdb.construct/target/org.bridgedb.rdb.construct-${VERSION}.jar:\
+${HOME}/.m2/repository/org/apache/derby/derby/10.5.3.0_1/derby-10.5.3.0_1.jar:\
+${HOME}/.m2/repository/com/google/collections/google-collections/1.0/google-collections-1.0.jar
+
+java -DstrictDataSourceChecking=false -cp ${CLASSPATH} \
+  org.bridgedb.tools.void.VoIDTool "$@"
