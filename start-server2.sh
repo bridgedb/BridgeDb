66862cb6e926bc7545912688d86bb5b98f5e5e5f
Egon Willighagen <egon.willighagen@gmail.com>
2019-11-19 10:57:38 +0100

    Updated the MySQL-Connector version

--- start-server2.sh
+++ start-server2.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-cd /home/wikipathways/code/bridgedb-2.1.0-SNAPSHOT/
+# FIXME: versions of BridgeDb and MySQL-Connector must be updated
+
+cd /home/wikipathways/code/bridgedb-3.0.0-SNAPSHOT/
 
 CLASSPATH=\
-org.bridgedb.server/lib/mysql-connector-java-5.1.7-bin.jar:\
+org.bridgedb.server/lib/mysql-connector-java-8.0.18-bin.jar:\
 dist/org.bridgedb.server.jar:\
 dist/commons-cli-1.2.jar
 
