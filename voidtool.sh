eb791f1edcbf145c6a2133db682d955d7c20a344
Egon Willighagen <egon.willighagen@gmail.com>
2020-08-22 10:25:29 +0200

    Fixed the package name

--- voidtool.sh
+++ voidtool.sh
@@ -12,4 +12,4 @@ ${HOME}/.m2/repository/org/apache/derby/derby/10.5.3.0_1/derby-10.5.3.0_1.jar:\
 ${HOME}/.m2/repository/com/google/collections/google-collections/1.0/google-collections-1.0.jar
 
 java -DstrictDataSourceChecking=false -cp ${CLASSPATH} \
-  org.bridgedb.tools.qc.VoIDTool "$@"
+  org.bridgedb.tools.voidtool.VoIDTool "$@"
