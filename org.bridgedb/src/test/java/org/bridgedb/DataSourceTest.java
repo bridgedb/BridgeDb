97bc643cb8d090a3f84252119b8b8eab389acd7e
Helena Basaric <h.basaric@maastrichtuniversity.nl>
2022-03-10 16:38:55 +0100

    Added unit tests
    Added unit tests to DataSourceTest.java.
    Increased code coverage.

--- org.bridgedb/src/test/java/org/bridgedb/DataSourceTest.java
+++ org.bridgedb/src/test/java/org/bridgedb/DataSourceTest.java
@@ -20,6 +20,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 
@@ -38,14 +45,59 @@ public class DataSourceTest {
 		DataSource source = DataSource.register("X", "Affymetrix").asDataSource();
 		assertEquals("X", source.getSystemCode());
 		assertEquals("Affymetrix", source.getFullName());
+		
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource.register("", ""); throw new IllegalArgumentException();
+				});
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource.register(null, ""); throw new IllegalArgumentException();
+				});
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource.register("X", ""); throw new IllegalArgumentException();
+				});
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource.register("X", null); throw new IllegalArgumentException();
+				});
+		
 	}
 
 	@org.junit.jupiter.api.Test
 	public void testBuildingMainUrl() {
 		DataSource source = DataSource.register("X", "Affymetrix")
 		    .mainUrl("http://www.affymetrix.com")
+		    .urlPattern(null)
 		    .asDataSource();
 		assertEquals("http://www.affymetrix.com", source.getMainUrl());
+		assertEquals(false, source.urlPatternKnown());
+		
+		DataSource source2 = DataSource.register("S", "Uniprot-TrEMBL")
+				.urlPattern("http://www.uniprot.org/uniprot/$id")
+			    .asDataSource();
+		
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource source3 = DataSource.register("S", "Uniprot-TrEMBL")
+							.urlPattern("http://www.uniprot.org/uniprot/$id")
+						    .asDataSource();
+					source3 = DataSource.register("S", "Uniprot-TrEMBL")
+							.urlPattern("http://www.uniprot.org/$id")
+							.asDataSource();
+					});
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource source6 = DataSource.register("S", "Uniprot-TrEMBL")
+							.urlPattern("http://www.uniprot.org/$id")
+							.asDataSource();
+					});
+
+		assertEquals(true, source2.urlPatternKnown());
+		assertEquals("http://identifiers.org/orphanet:$id/1234", source.getIdentifiersOrgUri("1234"));
+		assertEquals("orphanet:$id:1234", source.getCompactIdentifier("1234"));
+		
 	}
 
 	@org.junit.jupiter.api.Test
@@ -221,15 +273,51 @@ public class DataSourceTest {
 	public void testGetByPrefix() {
 		DataSource chebi = DataSource.register("Ce", "ChEBI")
 			.compactIdentifierPrefix("chebi")
+			.identifiersOrgBase("")
+			.urnBase(null)
+			.type(null)
+			.idExample("Ce")
+			.mainUrl(null)
 			.asDataSource();
 		assertNotNull(chebi);
+		
+		DataSource chebi2 = DataSource.register("Ce", "ChEBI")
+			.miriamBase("chebi")
+			.type("")
+			.identifiersOrgBase(null)
+			.mainUrl("")
+			.asDataSource();
+		assertNotNull(chebi2);
+		
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource.register("Ce", "ChEBI")
+					.miriamBase(null)
+					.asDataSource(); throw new IllegalArgumentException();
+				});
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource.register("Ce", "ChEBI")
+					.compactIdentifierPrefix(null)
+					.asDataSource(); throw new IllegalArgumentException();
+				});
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> {
+					DataSource.register("Ce", "ChEBI")
+					.urnBase("test")
+					.asDataSource(); throw new IllegalArgumentException();
+				});
+		
 		DataSource source = DataSource.getByCompactIdentifierPrefix("chebi");
 		
 		source = DataSource.register("Gpl", "Guide to Pharmacology")
 			    .asDataSource();
 		assertEquals(null, source.getMiriamURN(""));
-		assertEquals(null, source.getCompactIdentifier(""));
+		assertEquals(null, source.getCompactIdentifier("H"));
+		assertEquals(null, source.getCompactIdentifier("Gpl"));
+		
 		assertEquals(null, source.getIdentifiersOrgUri(""));
+
 		Assertions.assertThrows(IllegalArgumentException.class,
 				() -> {
 					source.getExistingBySystemCode(""); throw new IllegalArgumentException();
@@ -240,39 +328,73 @@ public class DataSourceTest {
 				() -> {
 					source.getExistingByFullName(""); throw new IllegalArgumentException();
 				});
-
 	}
-	
+    
+
 	@org.junit.jupiter.api.Test
 	public void testBuilders() {
 		
 		DataSource source = DataSource.register("X", "Affymetrix")
 				.type("probe")
+				.organism(null)
+				.alternative(null)
+				.urnBase("")
+				.identifiersOrgBase("http://identifiers.org/orphanet:$id")
 				.asDataSource();
 		assertEquals("probe",source.getType());
+		assertEquals(null,source.getOrganism());
 		assertEquals("Affymetrix",source.getExistingByFullName("Affymetrix").toString());
 		assertEquals(true,source.fullNameExists("Affymetrix"));
+		
 		DataSource source2 = DataSource.register("Eco", "Ecocyc")
+				.alternative("")
 				.organism("Escherichia coli")
+				.identifiersOrgBase("http://identifiers.org/orphanet/$id")
+				.urnBase("Eco")
 				.asDataSource();
 		assertEquals("Escherichia coli",source2.getOrganism());
 		assertNotNull(source2.getOrganism());
 			
 		DataSource source3 = DataSource.register("Ec", "Ecogene")
 				.urnBase("urn:miriam:ecogene")
+				.description(null)
+				.identifiersOrgBase("http://identifiers.org/ecogene/")
+				.alternative("MetaboLoci Alternative")
 				.asDataSource();
 		assertEquals("Ecogene",source3.getByMiriamBase("urn:miriam:ecogene").toString());
 		assertEquals("urn:miriam:ecogene:urn%3Amiriam%3Aecogene",source3.getMiriamURN("urn:miriam:ecogene").toString());
-		
+
 		DataSource source4 = DataSource.register("Ect", "EPA CompTox")
 					.urnBase("urn:miriam:Ect")
+					.description("")
 					.asDataSource();
 		assertEquals("EPA CompTox",source4.getByMiriamBase("urn:miriam:Ect").toString());
-			
+		assertEquals(null,source4.getByMiriamBase(null));
+		assertEquals(null,source4.getByMiriamBase("test"));
+		
 		source4.registerAlias("Ect");
 		assertEquals("EPA CompTox", source4.getByAlias("Ect").toString());
 		
 		source4.getExistingBySystemCode("Ect");
+		
+		DataSource source6 = DataSource.register("R", "RGD")
+				.organism("Rattus norvegicus")
+				.asDataSource();
+		source6 = DataSource.register("R", "RGD")
+				.organism("Rattus norvegicus")
+				.asDataSource();
+		assertEquals("Rattus norvegicus",source6.getOrganism());
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			DataSource source5 = DataSource.register("R", "RGD")
+					.organism("Rattus norvegicus")
+					.asDataSource();
+			source5 = DataSource.register("R", "RGD")
+					.organism("Rattus")
+					.asDataSource();
+			throw new IllegalArgumentException();
+		});
+		
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			DataSource source5 = DataSource.register("Ect", "EPA CompTox")
 					.urnBase("urn:miriam:Ect")
@@ -281,7 +403,100 @@ public class DataSourceTest {
 					.urnBase("urn:miriam:ecogene")
 					.asDataSource();
 			throw new IllegalArgumentException();
-		
 		});
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			DataSource source5 = DataSource.register("Ect", "EPA CompTox")
+					.alternative("Metaboloci Alternative")
+					.asDataSource();
+			source5 = DataSource.register("Ect", "EPA CompTox")
+					.alternative("test")
+					.asDataSource();
+			throw new IllegalArgumentException();
+		});
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			DataSource source5 = DataSource.register("Ect", "EPA CompTox")
+					.alternative("Metaboloci Alternative")
+					.asDataSource();
+			source5 = DataSource.register("Ect", "EPA CompTox")
+					.alternative("test")
+					.asDataSource();
+			throw new IllegalArgumentException();
+		});
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			DataSource source5 = DataSource.register("Ect", "EPA CompTox")
+					.description("EPA CompTox Dashboard")
+					.asDataSource();
+			source5 = DataSource.register("Ect", "EPA CompTox")
+					.description("test")
+					.asDataSource();
+			throw new IllegalArgumentException();
+		});
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			DataSource source5 = DataSource.register("Ect", "EPA CompTox")
+					.asDataSource();
+			source5 = DataSource.register("L", "EPA CompTox")
+					.asDataSource();
+			throw new IllegalArgumentException();
+		});
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			DataSource source5 = DataSource.register("Ect", "EPA CompTox")
+					.asDataSource();
+			source5 = DataSource.register(null, "EPA CompTox")
+					.asDataSource();
+			throw new IllegalArgumentException();
+		});
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			DataSource source5 = DataSource.register("Ec", "Ecogene")
+					.identifiersOrgBase("http://identifiers.org/ecogene/")
+					.asDataSource();
+			throw new IllegalArgumentException();
+		});
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			DataSource source5 = DataSource.register("Ec", "Ecogene")
+					.identifiersOrgBase("test")
+					.asDataSource();
+			throw new IllegalArgumentException();
+		});
+		
+		
 	}
+	
+	@org.junit.jupiter.api.Test
+	public void testGetters() {
+
+		Set<DataSource> set = DataSource.getDataSources();
+		assertNotNull(set);
+		
+		DataSource source = DataSource.register("Ect", "EPA CompTox")
+				.urnBase("urn:miriam:Ect")
+				.asDataSource();
+
+		
+		assertEquals(null, source.getKnownUrl("Ect"));
+		
+		assertNotNull(DataSource.getFilteredSet(false, false, source));
+		assertNotNull(DataSource.getFilteredSet(null, null, null));
+		assertNotNull(DataSource.getFilteredSet(true, true, source));
+		assertNotNull(DataSource.getFilteredSet(null, true, source));
+		assertNotNull(DataSource.getFilteredSet(null, false, source));
+		assertNotNull(DataSource.getFilteredSet(null, false, "Escherichia coli"));
+		
+		List<String> result = DataSource.getFullNames();
+		assertNotNull(result);
+		
+		assertEquals(null, DataSource.getByIdentiferOrgBase("base"));
+		assertEquals(null, DataSource.getByIdentiferOrgBase(null));
+		assertEquals(null, DataSource.getByIdentiferOrgBase("http://identifiers.org/ccds/"));
+		assertEquals(null, DataSource.getByIdentiferOrgBase("http://identifiers.org/ccds"));
+		assertNotNull(source.getCompactIdentifierPrefix());
+	}
+	
+	
 }
