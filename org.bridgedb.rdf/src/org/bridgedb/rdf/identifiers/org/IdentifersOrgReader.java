5386289a1df7f33462fd3b6011d7adc87d1c3a80
Egon Willighagen <egon.willighagen@gmail.com>
2024-07-25 16:54:57 +0200

    More precise status message

--- org.bridgedb.rdf/src/org/bridgedb/rdf/identifiers/org/IdentifersOrgReader.java
+++ org.bridgedb.rdf/src/org/bridgedb/rdf/identifiers/org/IdentifersOrgReader.java
@@ -92,7 +92,7 @@ public class IdentifersOrgReader extends RdfBase {
 //            for (String multiple:multiples){
 //                checkMultiple(repositoryConnection, multiple);
 //            }
-            Reporter.println("Registry read in. Now loading DataSources");
+            Reporter.println("Registry read in. Now loading DataSources from the in memory store");
             loadData(repositoryConnection);
         } catch (Exception ex) {
             throw new BridgeDBException ("Error parsing RDF inputStream: " + ex.getMessage(), ex);
@@ -204,7 +204,7 @@ public class IdentifersOrgReader extends RdfBase {
             UriPattern.register(identiferOrgBase + "$id", sysCode, UriPatternType.identifiersOrgPatternSimple);
         String identifersOrgInfoBase = identiferOrgBase.replace("identifiers.org","info.identifiers.org");
         UriPattern.register(identifersOrgInfoBase + "$id", sysCode, UriPatternType.identifiersOrgPatternInfo);
-             
+
         DataSource ds = DataSource.register(sysCode, fullName)
                 .identifiersOrgBase(identiferOrgBase)
                 .asDataSource();
