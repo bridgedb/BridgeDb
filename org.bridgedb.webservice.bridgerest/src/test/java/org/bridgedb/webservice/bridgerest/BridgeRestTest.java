2c6a62d8b952ca3898c29f083c591ebff5b461ec
Egon Willighagen <egon.willighagen@gmail.com>
2024-09-22 10:14:42 +0200

    Restructured and added a mapping test

--- org.bridgedb.webservice.bridgerest/src/test/java/org/bridgedb/webservice/bridgerest/BridgeRestTest.java
+++ org.bridgedb.webservice.bridgerest/src/test/java/org/bridgedb/webservice/bridgerest/BridgeRestTest.java
@@ -14,22 +14,36 @@
 //
 package org.bridgedb.webservice.bridgerest;
 
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Set;
+
+import org.bridgedb.DataSource;
 import org.bridgedb.IDMapperException;
+import org.bridgedb.Xref;
+import org.bridgedb.bio.DataSourceTxt;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class BridgeRestTest {
 
+	private static BridgeRest service;
+
 	@BeforeAll
-    public static void init() {
-		
+    public static void init() throws IDMapperException {
+		if (DataSource.getDataSources().size() == 0) DataSourceTxt.init();
+		BridgeRestTest.service = new BridgeRest("https://webservice.bridgedb.org/Human");
+		assertNotNull(service);
+		assertTrue(service.isConnected());
 	}
 
 	@Test
-	public void test() throws IDMapperException {
-		BridgeRest service = new BridgeRest("https://webservice.bridgedb.org/Human");
+	public void testMap() throws IDMapperException {
 		assertNotNull(service);
+		assertTrue(service.isConnected());
+		Set<Xref> mappings = service.mapID(new Xref("CHEBI:123", DataSource.getExistingBySystemCode("Ce")));
+		assertNotSame(0, mappings.size());
 	}
 }
