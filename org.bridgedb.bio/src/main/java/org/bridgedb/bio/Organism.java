40161f91b374ca415fe5f3528b060dbfd821ccf6
Egon Willighagen <egon.willighagen@gmail.com>
2025-11-09 14:17:22 +0100

    Added Salvia fruticosa

--- org.bridgedb.bio/src/main/java/org/bridgedb/bio/Organism.java
+++ org.bridgedb.bio/src/main/java/org/bridgedb/bio/Organism.java
@@ -97,6 +97,7 @@ public enum Organism {
 
 	RattusNorvegicus("Rattus norvegicus", "Rn", "Rat", 10116),
 	SaccharomycesCerevisiae("Saccharomyces cerevisiae", "Sc", "Yeast", 4932),
+	SalviaFruticosa("Salvia fruticosa", "Sf", "Greek sage", 268906),
 	SolanumLycopersicum("Solanum lycopersicum", "Sl", "Tomato", 4081),
 	SkeletonemaMarinoi("Skeletonema marinoi", "Sm", 267567),
 
