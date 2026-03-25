49f7b0ab6a60c4d2678bf276e21de2e771bc0b72
traybug23 <snehaprusty2330@gmail.com>
2026-03-25 11:12:44 +0000

    added the missing + in the return string

--- org.bridgedb.gui/src/org/bridgedb/gui/PgdbParameterModel.java
+++ org.bridgedb.gui/src/org/bridgedb/gui/PgdbParameterModel.java
@@ -43,7 +43,7 @@ public class PgdbParameterModel extends SimpleParameterModel implements BridgeDb
 	public String getHelpHtml()
 	{
 		return 
-			"<html><h1>BridgeDerby database" 
+			"<html><h1>BridgeDerby database" +
 			"<p>BridgeDerby are databases that consist of a single file which you can download " +
 			"to your computer for fast access. Once downloaded, BridgeDerby databases are much" +
 			"faster than a webservice." +
